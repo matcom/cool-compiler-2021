c2d833ac000c3ee2748ab4e3e873b2858c5bce01
Carmen I Cabrera <kr1000a@gmail.com>
2020-09-02 21:19:38 -0400

    Adding files to compute an LR1Parser

---
+++ cmp/tools.py
@@ -0,0 +1,184 @@
+from utils import ShiftReduceParser, ContainerSet
+from pycompiler import Item
+from automata import State
+
+
+def upd_table(table, head, symbol, production):
+    if not head in table:
+        table[head] = {}
+    if not symbol in table[head]:
+        table[head][symbol] = []
+    if production not in table[head][symbol]:
+        table[head][symbol].append(production)
+    return (len(table[head][symbol]) <= 1)
+
+def compute_local_first(firsts, alpha):
+    first_alpha = ContainerSet()
+
+    try:
+        alpha_is_epsilon = alpha.IsEpsilon
+    except:
+        alpha_is_epsilon = False
+
+    if alpha_is_epsilon:
+        first_alpha.set_epsilon()
+
+    else:
+        for symbol in alpha:
+            first_alpha.update(firsts[symbol])
+            if not firsts[symbol].contains_epsilon:
+                break
+        else:
+            first_alpha.set_epsilon()
+
+    return first_alpha
+
+def compute_firsts(G):
+    firsts = {}
+    change = True
+
+    for terminal in G.terminals:
+        firsts[terminal] = ContainerSet(terminal)
+
+    for nonterminal in G.nonTerminals:
+        firsts[nonterminal] = ContainerSet()
+
+    while change:
+        change = False
+
+        for production in G.Productions:
+            X = production.Left
+            alpha = production.Right
+
+            first_X = firsts[X]
+
+            try:
+                first_alpha = firsts[alpha]
+            except KeyError:
+                first_alpha = firsts[alpha] = ContainerSet()
+
+            local_first = compute_local_first(firsts, alpha)
+
+            change |= first_alpha.hard_update(local_first)
+            change |= first_X.hard_update(local_first)
+
+    return firsts
+
+
+
+def expand(item, firsts):
+    next_symbol = item.NextSymbol
+    if next_symbol is None or not next_symbol.IsNonTerminal:
+        return []
+
+    lookaheads = ContainerSet()
+    for preview in item.Preview():
+        lookaheads.hard_update(compute_local_first(firsts, preview))
+
+    assert not lookaheads.contains_epsilon
+    return [Item(prod, 0, lookaheads) for prod in next_symbol.productions]
+
+
+
+def compress(items):
+    centers = {}
+
+    for item in items:
+        center = item.Center()
+        try:
+            lookaheads = centers[center]
+        except KeyError:
+            centers[center] = lookaheads = set()
+        lookaheads.update(item.lookaheads)
+
+    return {Item(x.production, x.pos, set(lookahead)) for x, lookahead in centers.items()}
+
+
+def closure_lr1(items, firsts):
+    closure = ContainerSet(*items)
+
+    changed = True
+    while changed:
+        changed = False
+
+        new_items = ContainerSet()
+        for item in closure:
+            new_items.extend(expand(item, firsts))
+
+        changed = closure.update(new_items)
+
+    return compress(closure)
+
+
+def goto_lr1(items, symbol, firsts=None, just_kernel=False):
+    assert just_kernel or firsts is not None, '`firsts` must be provided if `just_kernel=False`'
+    items = frozenset(item.NextItem() for item in items if item.NextSymbol == symbol)
+    return items if just_kernel else closure_lr1(items, firsts)
+
+
+
+def build_LR1_automaton(G):
+    assert len(G.startSymbol.productions) == 1, 'Grammar must be augmented'
+
+    firsts = compute_firsts(G)
+    firsts[G.EOF] = ContainerSet(G.EOF)
+
+    start_production = G.startSymbol.productions[0]
+    start_item = Item(start_production, 0, lookaheads=(G.EOF,))
+    start = frozenset([start_item])
+
+    closure = closure_lr1(start, firsts)
+    automaton = State(frozenset(closure), True)
+
+    pending = [start]
+    visited = {start: automaton}
+
+    while pending:
+        current = pending.pop()
+        current_state = visited[current]
+
+        for symbol in G.terminals + G.nonTerminals:
+            items = current_state.state
+            kernel = goto_lr1(items, symbol, just_kernel=True)
+            if not kernel:
+                continue
+            try:
+                next_state = visited[kernel]
+            except KeyError:
+                closure = goto_lr1(items, symbol, firsts)
+                next_state = visited[kernel] = State(frozenset(closure), True)
+                pending.append(kernel)
+
+            current_state.add_transition(symbol.Name, next_state)
+
+    automaton.set_formatter(lambda x: "")
+    return automaton
+
+
+class LR1Parser(ShiftReduceParser):
+    def _build_parsing_table(self):
+        self.ok = True
+        G = self.Augmented = self.G.AugmentedGrammar(True)
+
+        automaton = self.automaton = build_LR1_automaton(G)
+        for i, node in enumerate(automaton):
+            if self.verbose: print(i, '\t', '\n\t '.join(str(x) for x in node.state), '\n')
+            node.idx = i
+            node.tag = f'I{i}'
+
+        for node in automaton:
+            idx = node.idx
+            for item in node.state:
+                if item.IsReduceItem:
+                    prod = item.production
+                    if prod.Left == G.startSymbol:
+                        self.ok &= upd_table(self.action, idx, G.EOF, (ShiftReduceParser.OK, ''))
+                    else:
+                        for lookahead in item.lookaheads:
+                            self.ok &= upd_table(self.action, idx, lookahead, (ShiftReduceParser.REDUCE, prod))
+                else:
+                    next_symbol = item.NextSymbol
+                    if next_symbol.IsTerminal:
+                        self.ok &= upd_table(self.action, idx, next_symbol, (ShiftReduceParser.SHIFT, node[next_symbol.Name][0].idx))
+                    else:
+                        self.ok &= upd_table(self.goto, idx, next_symbol, node[next_symbol.Name][0].idx)
