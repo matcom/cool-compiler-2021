d9429e969e55e937d21f5d01db6b9106a6be0867
Carmen I Cabrera <kr1000a@gmail.com>
2020-11-27 18:18:20 -0500

    refs #9 - Adding Inferencer Manager to Type Builder
    - An instance of the class Inferencer Manager is created.
    -Every time a new attribute or function is visited, if the auto_type is involved it creates a new id through the manager and it adds this id to the node and to the type is being checked

--- cmp/type_builder.py
+++ cmp/type_builder.py
@@ -1,6 +1,6 @@
 import cmp.visitor as visitor
 from cmp.ast import ProgramNode, ClassDeclarationNode, AttrDeclarationNode, FuncDeclarationNode
-from cmp.semantic import SemanticError, ErrorType
+from cmp.semantic import SemanticError, ErrorType, InferencerManager, AutoType
 
 
 class TypeBuilder:
@@ -8,6 +8,8 @@ class TypeBuilder:
         self.context = context
         self.current_type = None
         self.errors = errors
+        self.manager = InferencerManager(context)
+
     
     @visitor.on('node')
     def visit(self, node):
@@ -35,11 +37,15 @@ class TypeBuilder:
         ## Building param-names and param-types of the method
         param_names = []
         param_types = []
+        node.index = []
         for param in node.params:
             n, t = param
             param_names.append(n)
+            node.index.append(None)
             try:
                 t = self.context.get_type(t)
+                if isinstance(t, AutoType):
+                    node.index[-1] = self.manager.assign_id()
             except SemanticError as ex:
                 self.errors.append(ex.text)
                 t = ErrorType()
@@ -52,9 +58,11 @@ class TypeBuilder:
             self.errors.append(ex.text)
             rtype = ErrorType()
         
+        node.idx = self.manager.assign_id() if isinstance(rtype, AutoType) else None
+        
         # Defining the method in the current type. There can not be another method with the same name.
         try:
-            self.current_type.define_method(node.id, param_names, param_types, rtype)
+            self.current_type.define_method(node.id, param_names, param_types, rtype, node.index, node.idx)
         except SemanticError as ex:
             self.errors.append(ex.text)
             
@@ -67,11 +75,13 @@ class TypeBuilder:
         except SemanticError as ex:
             self.errors.append(ex.text)
             attr_type = ErrorType()
+
+        node.idx = self.manager.assign_id() if isinstance(attr_type, AutoType) else None
         
         # Checking attribute name. No other attribute can have the same name
         flag = False
         try:
-            self.current_type.define_attribute(node.id, attr_type)
+            self.current_type.define_attribute(node.id, attr_type, node.idx)
             flag = True
         except SemanticError as ex:
             self.errors.append(ex.text)
@@ -79,7 +89,7 @@ class TypeBuilder:
         while not flag:
             node.id = f'1{node.id}'
             try:
-                self.current_type.define_attribute(node.id, attr_type)
+                self.current_type.define_attribute(node.id, attr_type, node.idx)
                 flag = True
             except SemanticError:
                 pass
