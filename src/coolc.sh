5456eb8b120e4e24cd9eff15ce67951a827e25b8
ariel.plasencia <arielplasencia00@gmail.com>
2021-02-23 01:33:24 -0500

    Update makefile and coolc.sh

--- src/coolc.sh
+++ src/coolc.sh
@@ -1,10 +1,9 @@
 
-FILE="./main.py"
 INPUT_FILE=$1
 OUTPUT_FILE=${INPUT_FILE:0: -2}mips
 
-# echo "COOL Compiler v1.0.0"
-# echo "Copyright (c) 2021: Juan José López, Juan Carlos Esquivel, Ariel Plasencia"
+echo "COOL Compiler v1.0.0"
+echo "Copyright (c) 2021: Juan José López, Juan Carlos Esquivel, Ariel Plasencia"
 
 # echo "Compiling $INPUT_FILE into $OUTPUT_FILE"
-python $FILE $INPUT_FILE $OUTPUT_FILE
+python3 main.py $INPUT_FILE $OUTPUT_FILE
