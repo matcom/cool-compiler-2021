0977f803925373a67fc68072de070b38e693fe39
ariel.plasencia <arielplasencia00@gmail.com>
2021-02-24 00:05:31 -0500

    Change the version of COOL Compiler

--- src/coolc.sh
+++ src/coolc.sh
@@ -2,7 +2,7 @@
 INPUT_FILE=$1
 OUTPUT_FILE=${INPUT_FILE:0: -2}mips
 
-echo "COOL Compiler v1.0.0"
+echo "COOL Compiler v2.0.0"
 echo "Copyright (c) 2021: Juan José López, Juan Carlos Esquivel, Ariel Plasencia"
 
 # echo "Compiling $INPUT_FILE into $OUTPUT_FILE"
