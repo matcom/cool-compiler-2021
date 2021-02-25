c6ef91797108a7d1d689b4a34df25ce242c8446d
ariel.plasencia <arielplasencia00@gmail.com>
2021-02-25 00:27:53 -0500

    Add Context into semantic/tools.py

--- src/semantic/tools.py
+++ src/semantic/tools.py
@@ -1,4 +1,46 @@
+from utils.errors import *
+from semantic.types import Type
 
+class Context:
+    def __init__(self):
+        self.types = {}
+
+    def get_depth(self, class_name):
+        typex = self.types[class_name]
+        if typex.parent is None:
+            return 0
+        return 1 + self.get_depth(typex.parent.name)
+
+    def build_inheritance_graph(self):
+        graph = {}
+        for type_name, typex in self.types.items():
+            if typex.parent is not None:
+                graph[type_name] = typex.parent.name 
+            else:
+                if type_name == 'SELF_TYPE':
+                    continue
+                graph[type_name] = None
+        return graph
+
+    def create_type(self, name:str, pos) -> Type:
+        if name in self.types:
+            error_text = SemanticError.TYPE_ALREADY_DEFINED
+            raise SemanticError(*pos, error_text)
+        typex = self.types[name] = Type(name, pos)
+        return typex
+
+    def get_type(self, name:str, pos) -> Type:
+        try:
+            return self.types[name]
+        except KeyError:
+            error_text = TypesError.TYPE_NOT_DEFINED % name
+            raise TypesError(*pos, error_text)
+
+    def __str__(self):
+        return '{\n    ' + '\n    '.join(y for x in self.types.values() for y in str(x).split('\n')) + '\n}'
+
+    def __repr__(self):
+        return str(self)
 
 class VariableInfo:
     def __init__(self, name, vtype, index=None):
