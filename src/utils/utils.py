2186a7a205f2cc09a174e4725fadee0c34bd2a6d
ariel.plasencia <arielplasencia00@gmail.com>
2021-03-08 22:05:14 -0500

    Add class Utils into utils/utils.py

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,3 +1,7 @@
+import os
+import itertools
+from semantic.types import Type, SelfType
+
 class Token:
     def __init__(self, line, column, type, lex):
         self.line = line
@@ -10,3 +14,37 @@ class Token:
 
     def __repr__(self):
         return str(self)
+
+class Utils:
+
+    @staticmethod
+    def PathToObjet(typex):
+        path = []
+        c_type = typex
+
+        while c_type:
+            path.append(c_type)
+            c_type = c_type.parent
+
+        path.reverse()
+        return path
+
+    @staticmethod
+    def GetCommonBaseType(types):
+        paths = [ Utils.PathToObjet(typex) for typex in types ]
+        tuples = zip(*paths)
+
+        for i, t in enumerate(tuples):
+            gr = itertools.groupby(t)
+            if len(list(gr)) > 1:
+                return paths[0][i-1]
+
+        return paths[0][-1]
+
+    @staticmethod
+    def GetType(typex : Type, current_type : Type) -> Type:
+        return current_type if typex == SelfType() else typex
+
+    @staticmethod
+    def IsBasicType(type_name : str):
+        return type_name in ['String', 'Int', 'Object', 'Bool', 'SELF_TYPE', 'IO']
