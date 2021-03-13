9b018c6f621415d1ecb124dbcaa140b12f0185c7
jvaldesgonzalez <jvaldesgonzalez9@gmail.com>
2021-03-13 16:45:15 -0500

    refactor(custom-tests): Add utils module for compare results

--- src/modules/lexer/test/comments_test.py
+++ src/modules/lexer/test/comments_test.py
@@ -2,19 +2,13 @@ import pytest
 import os
 
 tests_dir = __file__.rpartition("/")[0] + "/input/comments/"
-tests = [(file) for file in os.listdir(tests_dir) if file.endswith(".cl")]
 
 from src.modules.lexer import CoolLexer
+from .utils import compare_results
 
 
 @pytest.mark.lexer
 @pytest.mark.error
 @pytest.mark.run(order=2)
 def test_should_eof_in_comment():
-    for test in tests:
-        fd = open(tests_dir + test)
-        code = fd.read()
-        lexer = CoolLexer(lexer_errors=[])
-        for token in lexer.tokenize(code):
-            pass
-        assert lexer.errors == ["(55, 46) - LexicographicError: EOF in comment"]
+    compare_results(tests_dir)
