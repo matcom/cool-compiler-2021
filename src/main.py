19a4b573da863b3e02215a2f0172e63900a32d29
Reinaldo14 <reinaldobarrera9601@gmail.com>
2021-04-21 20:08:34 +0000

    Return to begin

--- src/main.py
+++ src/main.py
@@ -1,13 +1,9 @@
 import sys
-from lexer.grammar import CoolGrammar
-
-def Lexer(input_file, output_file):
-    print(CoolGrammar())
 
 def main():
     input_file = sys.argv[1]
     output_file = input_file[:-2] + 'mips'
-    Lexer(input_file, output_file)
+
     exit(0)
 
 if __name__ == '__main__':
