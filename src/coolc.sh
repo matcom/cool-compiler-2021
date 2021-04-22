80ce9582e1aa7fab686d4c7fc43cd10e28653588
Reinaldo14 <reinaldobarrera9601@gmail.com>
2021-04-22 05:04:58 +0000

    Begin again

--- src/coolc.sh
+++ src/coolc.sh
@@ -1,9 +1,6 @@
-# Incluya aquí las instrucciones necesarias para ejecutar su compilador
-
 INPUT_FILE=$1
 
 echo "cool-compiler-2021"
-echo "Copyright (c) 2019: Reinaldo, Paula, Julio"
+echo "Copyright (c) 2019: Reinaldo Barrera Travieso"
 
-# Llamar al compilador
 python3 main.py $INPUT_FILE
