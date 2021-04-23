45fe7c63f231477e5ebc596580c35f727e633b08
RodroVMS <democlesvanshepars@gmail.com>
2021-04-23 13:00:13 -0400

    Minor changes to __init_.py

--- src/semantics/inference/__init__.py
+++ src/semantics/inference/__init__.py
@@ -1,4 +1,2 @@
-from .autotype_collector import AutotypeCollector
-from .autotype_inferencer import AutotypeInferencer
 from .soft_inferencer import SoftInferencer
 from .back_inferencer import BackInferencer
