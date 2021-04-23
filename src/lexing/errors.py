0f7f6167e7eb5f0e327b24d625f42ead4eb944cf
Adrian Portales <adrianportales135@gmail.com>
2021-04-23 12:57:03 -0400

    Fix error in LexicographicError class

--- src/lexing/errors.py
+++ src/lexing/errors.py
@@ -1,11 +1,11 @@
 class LexicographicError:
-    def __init__(self, line: int, col: int, char: str) -> None:
+    def __init__(self, line: int, col: int, message: str) -> None:
         self.line = line
         self.col = col
-        self.char = char
+        self.message = message
 
     def __str__(self) -> str:
-        return f'({self.line},{self.col}) - LexicographicError: ERROR "{self.char}"'
+        return f'({self.line},{self.col}) - LexicographicError: ERROR "{self.message}"'
 
     def __repr__(self) -> str:
         return str(self)
--- src/semantics/errors.py
+++ src/semantics/errors.py
@@ -1,22 +1,22 @@
 class InternalError(Exception):
     @property
     def text(self):
-        return "Internal Error: " + self.args[0]
+        return "InternalError: " + self.args[0]
 
 
 class SemanticError(Exception):
     @property
     def text(self):
-        return "Semantic Error: " + self.args[0]
+        return "SemanticError: " + self.args[0]
 
 
 class TypeError(SemanticError):
     @property
     def text(self):
-        return "Type Error: " + self.args[0]
+        return "TypeError: " + self.args[0]
 
 
 class AttributeError(SemanticError):
     @property
     def text(self):
-        return "Attribute Error: " + self.args[0]
+        return "AttributeError: " + self.args[0]
