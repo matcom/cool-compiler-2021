44792d2515581d25869205d8af125ecd05ac5aed
ariel.plasencia <arielplasencia00@gmail.com>
2021-04-27 22:26:14 -0400

    Update coolc.sh

--- src/coolc.sh
+++ src/coolc.sh
@@ -3,7 +3,7 @@ INPUT_FILE=$1
 OUTPUT_FILE=${INPUT_FILE:0: -2}mips
 
 echo "COOL Compiler v2.0.0"
-echo "Copyright (c) 2021: Juan José López, Juan Carlos Esquivel, Ariel Plasencia"
+echo "Copyright (©) 2021: Juan José López, Juan Carlos Esquivel, Ariel Plasencia"
 
 # echo "Compiling $INPUT_FILE into $OUTPUT_FILE"
 python3 main.py $INPUT_FILE $OUTPUT_FILE
