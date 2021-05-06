ba03cc76665fd783ffe0e6a8f6a8696ee8fccd0d
Adrian Portales <adrianportales135@gmail.com>
2021-05-06 17:50:02 -0400

    Added support for inferencing the more general type in parameters and the more concrete in the rest of expressions

--- src/semantics/inference/types_inferencer.py
+++ src/semantics/inference/types_inferencer.py
@@ -1,7 +1,7 @@
-from semantics.tools.type import ErrorType
+from semantics.tools.type import join_list
 from utils import visitor
 
-from semantics.tools import TypeBag
+from semantics.tools import TypeBag, Scope
 import ast.types_ast as types_ast
 from ast.inferencer_ast import (
     BinaryNode,
@@ -45,141 +45,168 @@ class TypesInferencer:
         self.errors = []
 
     @visitor.on("node")
-    def visit(self, node):
+    def visit(self, node, scope):
         pass
 
     @visitor.when(ProgramNode)
     def visit(self, node: ProgramNode) -> types_ast.ProgramNode:
         class_decl = []
+        scope = Scope()
         for decl in node.declarations:
-            class_decl.append(self.visit(decl))
+            class_decl.append(self.visit(decl, scope.create_child()))
 
         return types_ast.ProgramNode(class_decl, node)
 
     @visitor.when(ClassDeclarationNode)
-    def visit(self, node: ClassDeclarationNode) -> types_ast.ClassDeclarationNode:
+    def visit(self, node: ClassDeclarationNode, scope: Scope) -> types_ast.ClassDeclarationNode:
         features = []
         for feature in node.features:
-            features.append(self.visit(feature))
+            features.append(self.visit(feature, scope))
 
         return types_ast.ClassDeclarationNode(features, node)
 
     @visitor.when(AttrDeclarationNode)
-    def visit(self, node: AttrDeclarationNode) -> types_ast.AttrDeclarationNode:
+    def visit(self, node: AttrDeclarationNode, scope: Scope) -> types_ast.AttrDeclarationNode:
         new_node = types_ast.AttrDeclarationNode(node)
         if node.expr:
-            new_node.expr = self.visit(node.expr)
+            new_node.expr = self.visit(node.expr, scope)
         new_node.type = self._reduce_to_type(node.inferenced_type, node)
         return new_node
 
     @visitor.when(MethodDeclarationNode)
-    def visit(self, node: MethodDeclarationNode) -> types_ast.MethodDeclarationNode:
-        body = self.visit(node.body)
-        params = [self.visit(param) for param in node.params]
+    def visit(self, node: MethodDeclarationNode, scope: Scope) -> types_ast.MethodDeclarationNode:
+        scope = scope.create_child()
+        
+        for param in node.params:
+            param_type = self._reduce_to_type(param.inferenced_type, general=True)
+            scope.define_variable(param.id, param_type)
+
+        params = [self.visit(param, scope) for param in node.params]
+        
+        body = self.visit(node.body, scope)
+        
         ret_type = self._reduce_to_type(node.inferenced_type, node)
         return types_ast.MethodDeclarationNode(params, ret_type, body, node)
 
     @visitor.when(BlocksNode)
-    def visit(self, node: BlocksNode) -> types_ast.BlocksNode:
-        expr_list = [self.visit(expr) for expr in node.expr_list]
+    def visit(self, node: BlocksNode, scope: Scope) -> types_ast.BlocksNode:
+        expr_list = [self.visit(expr, scope) for expr in node.expr_list]
 
         new_node = types_ast.BlocksNode(expr_list, node)
         new_node.type = expr_list[-1].type
         return new_node
 
     @visitor.when(ConditionalNode)
-    def visit(self, node: ConditionalNode) -> types_ast.ConditionalNode:
-        condition = self.visit(node.condition)
-        then_body = self.visit(node.then_body)
-        else_body = self.visit(node.else_body)
+    def visit(self, node: ConditionalNode, scope: Scope) -> types_ast.ConditionalNode:
+        condition = self.visit(node.condition, scope)
+        then_body = self.visit(node.then_body, scope)
+        else_body = self.visit(node.else_body, scope)
 
         new_node = types_ast.ConditionalNode(condition, then_body, else_body, node)
         new_node.type = self._reduce_to_type(node.inferenced_type, node)
         return new_node
 
     @visitor.when(CaseNode)
-    def visit(self, node: CaseNode) -> types_ast.CaseNode:
-        expr = self.visit(node.case_expr)
-        case_options = [self.visit(option) for option in node.options]
+    def visit(self, node: CaseNode, scope: Scope) -> types_ast.CaseNode:
+        expr = self.visit(node.case_expr, scope)
+        
+        case_options = []
+        for option in node.options:
+            child_scope = scope.create_child()
+            case_options.append(self.visit(option, child_scope))
+
         new_node = types_ast.CaseNode(expr, case_options, node)
         new_node.type = self._reduce_to_type(node.inferenced_type, node)
         return new_node
 
     @visitor.when(CaseOptionNode)
-    def visit(self, node: CaseOptionNode) -> types_ast.CaseOptionNode:
-        return types_ast.CaseOptionNode(self.visit(node.expr), node)
+    def visit(self, node: CaseOptionNode, scope: Scope) -> types_ast.CaseOptionNode:
+        return types_ast.CaseOptionNode(self.visit(node.expr, scope), node)
 
     @visitor.when(LoopNode)
-    def visit(self, node: LoopNode) -> types_ast.LoopNode:
-        condition = self.visit(node.condition)
-        body = self.visit(node.body)
+    def visit(self, node: LoopNode, scope: Scope) -> types_ast.LoopNode:
+        condition = self.visit(node.condition, scope)
+        body = self.visit(node.body, scope)
         new_node = types_ast.LoopNode(condition, body, node)
         new_node.type = self._reduce_to_type(node.inferenced_type, node)
         return new_node
 
     @visitor.when(LetNode)
-    def visit(self, node: LetNode) -> types_ast.LetNode:
-        var_decl_list = [self.visit(var_decl) for var_decl in node.var_decl_list]
-        in_expr = self.visit(node.in_expr)
+    def visit(self, node: LetNode, scope: Scope) -> types_ast.LetNode:
+        scope = scope.create_child()
+        var_decl_list = [self.visit(var_decl, scope) for var_decl in node.var_decl_list]
+        in_expr = self.visit(node.in_expr,scope)
         new_node = types_ast.LetNode(var_decl_list, in_expr, node)
         new_node.type = self._reduce_to_type(node.inferenced_type, node)
         return new_node
 
     @visitor.when(VarDeclarationNode)
-    def visit(self, node: VarDeclarationNode) -> types_ast.VarDeclarationNode:
+    def visit(self, node: VarDeclarationNode, scope: Scope) -> types_ast.VarDeclarationNode:
         new_node = types_ast.VarDeclarationNode(node)
         if node.expr:
-            new_node.expr = self.visit(node.expr)
-        new_node.type = self._reduce_to_type(node.inferenced_type, node)
+            new_node.expr = self.visit(node.expr, scope)
+
+        var_info = scope.find_variable(node.id)
+        if var_info is None:
+            new_node.type = self._reduce_to_type(node.inferenced_type, node)
+        else:
+            new_node.type = var_info.type
+
         return new_node
 
     @visitor.when(AssignNode)
-    def visit(self, node: AssignNode) -> types_ast.AssignNode:
-        expr = self.visit(node.expr)
+    def visit(self, node: AssignNode, scope: Scope) -> types_ast.AssignNode:
+        expr = self.visit(node.expr, scope)
         new_node = types_ast.AssignNode(expr, node)
         new_node.type = self._reduce_to_type(node.inferenced_type, node)
         return new_node
 
     @visitor.when(MethodCallNode)
-    def visit(self, node: MethodCallNode) -> types_ast.MethodCallNode:
-        args = [self.visit(arg) for arg in node.args]
-        caller_expr = self.visit(node.expr)
+    def visit(self, node: MethodCallNode, scope: Scope) -> types_ast.MethodCallNode:
+        args = [self.visit(arg, scope) for arg in node.args]
+        caller_expr = self.visit(node.expr, scope)
         new_node = types_ast.MethodCallNode(node.caller_type, caller_expr, args, node)
         new_node.type = self._reduce_to_type(node.inferenced_type, node)
         return new_node
 
     @visitor.when(VariableNode)
-    def visit(self, node: VariableNode) -> types_ast.VariableNode:
+    def visit(self, node: VariableNode, scope: Scope) -> types_ast.VariableNode:
         new_node = types_ast.VariableNode(node)
+        
         if node.defined:
-            new_node.type = self._reduce_to_type(node.inferenced_type, node)
+            var_info = scope.find_variable(node.value)
+            if var_info is None:
+                new_node.type = self._reduce_to_type(node.inferenced_type, node)
+            else:
+                new_node.type = var_info.type
+
         return new_node
 
     @visitor.when(IsVoidNode)
-    def visit(self, node: IsVoidNode) -> types_ast.IsVoidNode:
-        expr = self.visit(node.expr)
+    def visit(self, node: IsVoidNode, scope: Scope) -> types_ast.IsVoidNode:
+        expr = self.visit(node.expr, scope)
         new_node = types_ast.IsVoidNode(expr, node)
         new_node.type = self._reduce_to_type(node.inferenced_type, node)
         return new_node
 
     @visitor.when(NotNode)
-    def visit(self, node: NotNode) -> types_ast.NotNode:
-        expr = self.visit(node.expr)
+    def visit(self, node: NotNode, scope: Scope) -> types_ast.NotNode:
+        expr = self.visit(node.expr, scope)
         new_node = types_ast.NotNode(expr, node)
         new_node.type = self._reduce_to_type(node.inferenced_type, node)
         return new_node
 
     @visitor.when(ComplementNode)
-    def visit(self, node: ComplementNode) -> types_ast.ComplementNode:
+    def visit(self, node: ComplementNode, scope: Scope) -> types_ast.ComplementNode:
         expr = self.visit(node.expr)
         new_node = types_ast.ComplementNode(expr, node)
         new_node.type = self._reduce_to_type(node.inferenced_type, node)
         return new_node
 
     @visitor.when(BinaryNode)
-    def visit(self, node) -> types_ast.BinaryNode:
-        left = self.visit(node.left)
-        right = self.visit(node.right)
+    def visit(self, node, scope: Scope) -> types_ast.BinaryNode:
+        left = self.visit(node.left, scope)
+        right = self.visit(node.right, scope)
 
         if isinstance(node, PlusNode):
             new_node = types_ast.PlusNode(left, right, node)
@@ -200,30 +227,30 @@ class TypesInferencer:
         return new_node
 
     @visitor.when(BooleanNode)
-    def visit(self, node: BooleanNode) -> types_ast.BooleanNode:
+    def visit(self, node: BooleanNode, scope: Scope) -> types_ast.BooleanNode:
         new_node = types_ast.BooleanNode(node)
         new_node.type = self._reduce_to_type(node.inferenced_type, node)
         return new_node
 
     @visitor.when(StringNode)
-    def visit(self, node: StringNode) -> types_ast.StringNode:
+    def visit(self, node: StringNode, scope: Scope) -> types_ast.StringNode:
         new_node = types_ast.StringNode(node)
         new_node.type = self._reduce_to_type(node.inferenced_type, node)
         return new_node
 
     @visitor.when(IntNode)
-    def visit(self, node: IntNode) -> types_ast.IntNode:
+    def visit(self, node: IntNode, scope: Scope) -> types_ast.IntNode:
         new_node = types_ast.IntNode(node)
         new_node.type = self._reduce_to_type(node.inferenced_type, node)
         return new_node
 
     @visitor.when(InstantiateNode)
-    def visit(self, node: InstantiateNode) -> types_ast.InstantiateNode:
+    def visit(self, node: InstantiateNode, scope: Scope) -> types_ast.InstantiateNode:
         new_node = types_ast.InstantiateNode(node)
         new_node.type = self._reduce_to_type(node.inferenced_type, node)
         return new_node
 
-    def _reduce_to_type(self, bag: TypeBag, node: Node):
+    def _reduce_to_type(self, bag: TypeBag, node: Node, general=False):
         if len(bag.heads) > 1:
             self.add_error(
                 node,
@@ -233,7 +260,20 @@ class TypesInferencer:
         if len(bag.heads) == 0:
             self.add_error(node, "TypeError: Cannot infer expression's type")
             return TypeBag(set())
-        return bag.heads[0]
+
+        if general:
+            return bag.heads[0]
+
+        order_types = list(bag.type_set)
+        order_types.sort(key=lambda x: -1 * x.index)
+
+        higher_index_types = [order_types[0]]
+        higher_index = order_types[0].index
+        for typex in order_types[2:]:
+            if typex.index == higher_index:
+                higher_index_types.append(typex)
+
+        return join_list(higher_index_types)
 
     def add_error(self, node, text: str):
         line, col = node.get_position() if node else (0, 0)
