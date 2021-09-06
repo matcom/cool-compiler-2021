530b10e5c0a1aa86a4af7eb66a6b777687f946a4
cwjki <cwjki97@gmail.com>
2021-09-06 16:00:05 -0400

    working on lexer.py

--- src/1_sintactic/lexer.py
+++ src/1_sintactic/lexer.py
@@ -1,59 +1,110 @@
 import ply.lex as lex
 
 
-def find_column(input, lexpos):
-    line_start = input.rfind('\n', 0, lexpos) + 1
-    return (lexpos - line_start) + 1
 
 
 class CoolLexer:
 
+    reserved = {
+        'class': 'CLASS',
+        'else': 'ELSE',
+        'false': 'FALSE',
+        'fi': 'FI',
+        'if': 'IF',
+        'in': 'IN',
+        'inherits': 'INHERITS',
+        'isvoid': 'ISVOID',
+        'let': 'LET',
+        'loop': 'LOOP',
+        'pool': 'POOL',
+        'then': 'THEN',
+        'while': 'WHILE',
+        'case': 'CASE',
+        'esac': 'ESAC',
+        'new': 'NEW',
+        'of': 'OF',
+        'not': 'NOT'
+        'true': 'TRUE'
+    }
 
-reserved = {
-    'class': 'CLASS',
-    'else': 'ELSE',
-    'false': 'FALSE',
-    'fi': 'FI',
-    'if': 'IF',
-    'in': 'IN',
-    'inherits': 'INHERITS',
-    'isvoid': 'ISVOID',
-    'let': 'LET',
-    'loop': 'LOOP',
-    'pool': 'POOL',
-    'then': 'THEN',
-    'while': 'WHILE',
-    'case': 'CASE',
-    'esac': 'ESAC',
-    'new': 'NEW',
-    'of': 'OF',
-    'not': 'NOT'
-    'true': 'TRUE'
-}
+    tokens = [
+        'ID',
+        'TYPE',
+        'LPAREN',
+        'RPAREN',
+        'LBRACE',
+        'RBRACE',
+        'COLON',
+        'SEMICOLON',
+        'COMMA',
+        'DOT',
+        'AT',
+        'ASSIGN',
+        'PLUS',
+        'MINUS',
+        'STAR',
+        'DIV',
+        'EQUAL',
+        'LESS',
+        'LESSEQ',
+        'ARROW',
+        'INT',
+        'STRING',
+        'BOOLEAN',
+        'NOT'
+    ] + list(reserved.values())
 
-tokens = [
-    'ID',
-    'TYPE',
-    'LPAREN',
-    'RPAREN',
-    'LBRACE',
-    'RBRACE',
-    'COLON',
-    'SEMICOLON',
-    'COMMA',
-    'DOT',
-    'AT',
-    'ASSIGN',
-    'PLUS',
-    'MINUS',
-    'STAR',
-    'DIV',
-    'EQUAL',
-    'LESS',
-    'LESSEQ',
-    'ARROW',
-    'INT',
-    'STRING',
-    'BOOLEAN',
-    'NOT'
-]
+
+
+    def find_column(self, t):
+        line_start = self.text.rfind('\n', 0, t.lexpos) + 1
+        return (t.lexpos - line_start) + 1
+
+    def add_row_column(self, t):
+        t.row = t.lexer.lineno
+        t.column = self.find_column(t)
+
+
+
+
+
+
+    def t_LPAREN(self, t):
+        r'\('
+        self.add_row_column(t)
+        return t
+    
+    def t_RPAREN(self, t):
+        r'\)'
+        self.add_row_column(t)
+        return t
+
+    def t_LBRACE(self, t):
+        r'\{'
+        self.add_row_column(t)
+        return t
+    
+    def t_RPAREN(self, t):
+        r'\}'
+        self.add_row_column(t)
+        return t
+
+    def t_COLON(self, t):
+        r':'
+        self.add_row_column(t)
+        return t
+
+    def t_SEMICOLON(self, t):
+        r';'
+        self.add_row_column(t)
+        return t
+
+    def t_DOT(self, t):
+        r'\.'
+        self.add_row_column(t)
+        return t
+
+
+
+
+
