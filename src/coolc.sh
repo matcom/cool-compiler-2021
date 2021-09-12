122e9d0f20cbb76b675fdf68013350ab9a7675f2
Reinaldo14 <reinaldobarrera9601@gmail.com>
2021-09-12 16:14:07 +0000

    init lexer

--- src/coolc.sh
+++ src/coolc.sh
@@ -1,9 +1,11 @@
-#!/bin/bash
+# Incluya aquí las instrucciones necesarias para ejecutar su compilador
 
 INPUT_FILE=$1
 OUTPUT_FILE=${INPUT_FILE:0: -2}mips
 
+# Si su compilador no lo hace ya, aquí puede imprimir la información de contacto
 echo "cool-compiler-2021"
 echo "Copyright (c) 2021: Reinaldo Barrera Travieso"
 
-python3 main.py $INPUT_FILE $OUTPUT_FILE
+# Llamar al compilador
+python3 $INPUT_FILE $OUTPUT_FILE
