9db81e500e8faa8d0cbf5b9912ed9c4a9507764c
cwjki <cwjki97@gmail.com>
2021-10-11 15:33:15 -0400

    adding some nodes to ast.py

--- src/1_sintactic/ast.py
+++ src/1_sintactic/ast.py
@@ -45,3 +45,50 @@ class AssignNode(ExpressionNode):
         super().__init__()
         self.name = name
         self.expr = expr
+
+
+# FUNC CALLS NODES MISSING
+
+
+class IfNode(ExpressionNode):
+    def __init__(self, condition, then_expr, else_expr):
+        super().__init__()
+        self.condition = condition
+        self.then_expr = then_expr
+        self.else_expr = else_expr
+
+
+class WhileNode(ExpressionNode):
+    def __init__(self, condition, body):
+        super().__init__()
+        self.condition = condition
+        self.body = body
+
+
+class BlockNode(ExpressionNode):
+    def __init__(self, exprs):
+        super().__init__()
+        self.exprs = exprs
+
+
+class LetNode(ExpressionNode):
+    def __init__(self, let_attrs, expr):
+        super().__init__()
+        self.let_attrs = let_attrs
+        self.expr = expr
+
+
+class CaseNode(ExpressionNode):
+    def __init__(self, expr, case_list):
+        super().__init__()
+        self.expr = expr
+        self.case_list = case_list:
+
+
+class CaseOptionNode(ExpressionNode):
+    def __init__(self, id, expr, type):
+        super().__init__()
+        self.id = id
+        self.expr = expr
+        self.type = type
+        
