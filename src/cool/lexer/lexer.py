957ae58ef33e5e5a95ebcca099e95ac35aad4e73
JCH97 <jc1997hdez@gmail.com>
2021-10-12 14:49:36 -0400

    fix(lexer): remove print tokens

--- src/cool/lexer/lexer.py
+++ src/cool/lexer/lexer.py
@@ -15,10 +15,10 @@ class CoolLexer:
         self.lexer = lex.lex(self, **kwargs)
         self.lexer.lineno = 1
         self.lexer.linestart = 0
- 
+
     def updateColumn(self, t):
         t.column = t.lexpos - t.lexer.linestart + 1
-        
+
     states = (
         ('comments', 'exclusive'),
         ('strings', 'exclusive')
@@ -28,9 +28,9 @@ class CoolLexer:
     def t_comment(self, t):
         r'--.*($|\n)'
         t.lexer.lineno += 1
-        t.lexer.linestart = t.lexer.lexpos 
+        t.lexer.linestart = t.lexer.lexpos
 
-    def t_comments(self,t):
+    def t_comments(self, t):
         r'\(\*'
         t.lexer.level = 1
         t.lexer.begin('comments')
@@ -38,7 +38,7 @@ class CoolLexer:
     def t_comments_open(self, t):
         r'\(\*'
         t.lexer.level += 1
- 
+
     def t_comments_close(self, t):
         r'\*\)'
         t.lexer.level -= 1
@@ -59,9 +59,9 @@ class CoolLexer:
     def t_comments_eof(self, t):
         self.updateColumn(t)
         if t.lexer.level > 0:
-            error_text = errors.LexicographicError.EOF_COMMENT 
+            error_text = errors.LexicographicError.EOF_COMMENT
             self.errors.append(errors.LexicographicError(error_text, t.lineno, t.column))
-    
+
     # Strings
     t_strings_ignore = ''
 
@@ -76,21 +76,21 @@ class CoolLexer:
         r'\"'
         self.updateColumn(t)
 
-        if t.lexer.backslash : 
+        if t.lexer.backslash:
             t.lexer.myString += '"'
             t.lexer.backslash = False
         else:
-            t.value = t.lexer.myString 
+            t.value = t.lexer.myString
             t.type = 'string'
-            t.lexer.begin('INITIAL')           
+            t.lexer.begin('INITIAL')
             return t
 
     def t_strings_newline(self, t):
         r'\n'
         t.lexer.lineno += 1
         self.updateColumn(t)
-        
-        t.lexer.linestart = t.lexer.lexpos 
+
+        t.lexer.linestart = t.lexer.lexpos
 
         if not t.lexer.backslash:
             error_text = errors.LexicographicError.UNDETERMINED_STRING
@@ -101,36 +101,36 @@ class CoolLexer:
         r'\0'
         error_text = errors.LexicographicError.NULL_STRING
         self.updateColumn(t)
-  
+
         self.errors.append(errors.LexicographicError(error_text, t.lineno, t.column))
 
     def t_strings_consume(self, t):
         r'[^\n]'
 
-        if t.lexer.backslash :
+        if t.lexer.backslash:
             if t.value in ['b', 't', 'f', 'n', '\\']:
                 t.lexer.myString += f'\{t.value}'
-            else: 
-                t.lexer.myString += t.value 
+            else:
+                t.lexer.myString += t.value
 
             t.lexer.backslash = False
         else:
             if t.value != '\\':
                 t.lexer.myString += t.value
             else:
-                t.lexer.backslash = True 
+                t.lexer.backslash = True
 
     def t_strings_error(self, t):
         pass
 
     def t_strings_eof(self, t):
         self.updateColumn(t)
-     
+
         error_text = errors.LexicographicError.EOF_STRING
         self.errors.append(errors.LexicographicError(error_text, t.lineno, t.column))
-    
+
     t_ignore = '  \t\f\r\t\v'
-    
+
     def t_semi(self, t):
         r';'
         self.updateColumn(t)
@@ -150,32 +150,32 @@ class CoolLexer:
         r'\.'
         self.updateColumn(t)
         return t
- 
+
     def t_opar(self, t):
         r'\('
         self.updateColumn(t)
         return t
-    
+
     def t_cpar(self, t):
         r'\)'
         self.updateColumn(t)
         return t
-    
+
     def t_ocur(self, t):
         r'\{'
         self.updateColumn(t)
         return t
- 
+
     def t_ccur(self, t):
         r'\}'
         self.updateColumn(t)
         return t
- 
+
     def t_larrow(self, t):
         r'<-'
         self.updateColumn(t)
         return t
-    
+
     def t_arroba(self, t):
         r'@'
         self.updateColumn(t)
@@ -190,47 +190,47 @@ class CoolLexer:
         r'~'
         self.updateColumn(t)
         return t
- 
+
     def t_equal(self, t):
         r'='
         self.updateColumn(t)
         return t
- 
+
     def t_plus(self, t):
         r'\+'
         self.updateColumn(t)
         return t
- 
+
     def t_of(self, t):
         r'of'
         self.updateColumn(t)
         return t
- 
+
     def t_minus(self, t):
         r'-'
         self.updateColumn(t)
         return t
- 
+
     def t_star(self, t):
         r'\*'
         self.updateColumn(t)
         return t
- 
+
     def t_div(self, t):
         r'/'
         self.updateColumn(t)
         return t
-   
+
     def t_lesseq(self, t):
         r'<='
         self.updateColumn(t)
         return t
- 
+
     def t_less(self, t):
         r'<'
         self.updateColumn(t)
         return t
- 
+
     def t_inherits(self, t):
         r'inherits'
         self.updateColumn(t)
@@ -260,36 +260,38 @@ class CoolLexer:
     def t_newline(self, t):
         r'\n+'
         t.lexer.lineno += len(t.value)
-        t.lexer.linestart = t.lexer.lexpos 
+        t.lexer.linestart = t.lexer.lexpos
 
-    # Error handling rule
+        # Error handling rule
+
     def t_error(self, t):
         self.updateColumn(t)
         error_text = errors.LexicographicError.UNKNOWN_TOKEN % t.value[0]
-        
+
         self.errors.append(errors.LexicographicError(error_text, t.lineno, t.column))
-        #print(f'Report Error {len(self.errors)} {self.errors[0]}')
+        # print(f'Report Error {len(self.errors)} {self.errors[0]}')
         t.lexer.skip(1)
-        #t.lexer.skip(len(t.value))
-    
+        # t.lexer.skip(len(t.value))
+
     def tokenize(self, text: str) -> List[Token]:
         self.lexer.input(text)
         tokens: List[Token] = []
 
         for t in self.lexer:
             tokens.append(Token(t.type, t.value, t.lineno, t.column))
-        
+
         return tokens
 
-def main(text: str, output = None):
+
+def main(text: str, output=None):
     lexer = CoolLexer()
 
     # print(f'Path: {input}')
 
     a = lexer.tokenize(text)
-    
-    for i in a:
-        print(i)
+
+    # for i in a:
+    #     print(i)
 
     if lexer.errors:
         for e in lexer.errors:
@@ -297,7 +299,7 @@ def main(text: str, output = None):
         raise Exception()
 
     return lexer
-    
+
 
 if __name__ == '__main__':
     main()
--- src/cool/parser/parser.py
+++ src/cool/parser/parser.py
@@ -9,27 +9,27 @@ from ..utils.tokens import tokens
 
 
 class Parser:
-    def __init__(self, lexer = None):
+    def __init__(self, lexer=None):
         self.lexer = lexer if lexer else CoolLexer()
         self.outputdir = 'parser/output'
         self.tokens = tokens
         self.errors = False
-        self.parser = yacc.yacc(start = 'program',
-                                module = self, 
-                                outputdir = self.outputdir,
-                                optimize = 1,
-                                debuglog = log,
-                                errorlog = log)
+        self.parser = yacc.yacc(start='program',
+                                module=self,
+                                outputdir=self.outputdir,
+                                optimize=1,
+                                debuglog=log,
+                                errorlog=log)
 
-    
-    def parse(self, program, debug = False):
+    def parse(self, program, debug=False):
         self.errors = False
-        return self.parser.parse(program, self.lexer.lexer, debug = log)
+        return self.parser.parse(program, self.lexer.lexer, debug=log)
+
 
 class CoolParser(Parser):
     def p_program(self, p):
         'program : class_list'
- 
+
         p[0] = ProgramNode(p[1])
 
     def p_class_list(self, p):
@@ -37,25 +37,22 @@ class CoolParser(Parser):
                       | def_class'''
 
         p[0] = [p[1]] if len(p) == 2 else [p[1]] + p[2]
-    
+
     def p_class_list_error(self, p):
         'class_list : error class_list'
 
         p[0] = [p[1]] if len(p) == 2 else [p[1]] + p[2]
-        
 
     def p_epsilon(self, p):
         'epsilon :'
         pass
-    
 
     def p_def_class(self, p):
         '''def_class : class type ocur feature_list ccur semi 
                      | class type inherits type ocur feature_list ccur semi'''
 
         p[0] = ClassDeclarationNode(p.slice[2], p[4]) if len(p) == 7 else \
-               ClassDeclarationNode(p.slice[2], p[6], p.slice[4])
-    
+            ClassDeclarationNode(p.slice[2], p[6], p.slice[4])
 
     def p_def_class_error(self, p):
         '''def_class : class error ocur feature_list ccur semi 
@@ -67,7 +64,6 @@ class CoolParser(Parser):
 
         p[0] = ErrorNode()
 
-
     def p_feature_list(self, p):
         '''feature_list : epsilon
                         | def_attr semi feature_list
@@ -75,19 +71,17 @@ class CoolParser(Parser):
 
         p[0] = [] if len(p) == 2 else [p[1]] + p[3]
 
-
     def p_feature_list_error(self, p):
         'feature_list : error feature_list'
 
         p[0] = [p[1]] + p[2]
-
 
     def p_def_attr(self, p):
         '''def_attr : id colon type
                     | id colon type larrow expr'''
 
         p[0] = AttrDeclarationNode(p.slice[1], p.slice[3]) if len(p) == 4 else \
-               AttrDeclarationNode(p.slice[1], p.slice[3], p[5])
+            AttrDeclarationNode(p.slice[1], p.slice[3], p[5])
 
     def p_def_attr_error(self, p):
         '''def_attr : error colon type
@@ -95,13 +89,12 @@ class CoolParser(Parser):
                     | error colon type larrow expr
                     | id colon error larrow expr
                     | id colon type larrow error'''
-        
-        p[0] = ErrorNode()
 
+        p[0] = ErrorNode()
 
     def p_def_func(self, p):
         'def_func : id opar formals cpar colon type ocur expr ccur'
-        
+
         p[0] = FuncDeclarationNode(p.slice[1], p[3], p.slice[6], p[8])
 
     def p_def_func_error(self, p):
@@ -112,13 +105,11 @@ class CoolParser(Parser):
 
         p[0] = ErrorNode()
 
-
     def p_formals(self, p):
         '''formals : param_list
                    | param_list_empty'''
 
         p[0] = p[1]
-
 
     def p_param_list(self, p):
         '''param_list : param
@@ -126,17 +117,15 @@ class CoolParser(Parser):
 
         p[0] = [p[1]] if len(p) == 2 else [p[1]] + p[3]
 
-
     def p_param_list_empty(self, p):
         'param_list_empty : epsilon'
-        
+
         p[0] = []
 
     def p_param(self, p):
         'param : id colon type'
-        
-        p[0] = (p.slice[1], p.slice[3])
 
+        p[0] = (p.slice[1], p.slice[3])
 
     def p_let_list(self, p):
         '''let_list : let_assign
@@ -148,9 +137,8 @@ class CoolParser(Parser):
         '''let_assign : param larrow expr
                       | param'''
 
-        p[0] = VarDeclarationNode(p[1][0], p[1][1]) if len(p) ==  2 else\
-               VarDeclarationNode(p[1][0], p[1][1], p[3])
-
+        p[0] = VarDeclarationNode(p[1][0], p[1][1]) if len(p) == 2 else \
+            VarDeclarationNode(p[1][0], p[1][1], p[3])
 
     def p_cases_list(self, p):
         '''cases_list : casep semi
@@ -166,16 +154,14 @@ class CoolParser(Parser):
 
     def p_case(self, p):
         'casep : id colon type rarrow expr'
-        
-        p[0] = OptionNode(p.slice[1], p.slice[3], p[5])
 
+        p[0] = OptionNode(p.slice[1], p.slice[3], p[5])
 
     def p_expr(self, p):
         '''expr : id larrow expr
                 | comp'''
 
         p[0] = AssignNode(p.slice[1], p[3]) if len(p) == 4 else p[1]
-
 
     def p_comp(self, p):
         '''comp : comp less op
@@ -203,7 +189,7 @@ class CoolParser(Parser):
             p[0] = PlusNode(p[1], p[3])
         elif p[2] == '-':
             p[0] = MinusNode(p[1], p[3])
- 
+
     def p_term(self, p):
         '''term : term star base_call
                 | term div base_call
@@ -213,16 +199,14 @@ class CoolParser(Parser):
             p[0] = p[1]
         elif p[2] == '*':
             p[0] = StarNode(p[1], p[3])
-        elif p[2] == '/': 
+        elif p[2] == '/':
             p[0] = DivNode(p[1], p[3])
-
 
     def p_term_error(self, p):
         '''term : term star error
                 | term div error'''
 
         p[0] = ErrorNode()
-      
 
     def p_base_call(self, p):
         '''base_call : factor arroba type dot func_call
@@ -230,19 +214,18 @@ class CoolParser(Parser):
 
         p[0] = p[1] if len(p) == 2 else BaseCallNode(p[1], p.slice[3], *p[5])
 
-
     def p_base_call_error(self, p):
         '''base_call : error arroba type dot func_call
                      | factor arroba error dot func_call'''
 
         p[0] = ErrorNode()
 
-    def p_factor1(self, p):        
+    def p_factor1(self, p):
         '''factor : atom
                   | opar expr cpar'''
 
         p[0] = p[1] if len(p) == 2 else p[2]
-        
+
     def p_factor2(self, p):
         '''factor : factor dot func_call
                   | not expr
@@ -261,39 +244,34 @@ class CoolParser(Parser):
 
         p[0] = IsVoidNode(p[2], p.slice[1]) if p[1] == 'isvoid' else BinaryNotNode(p[2], p.slice[1])
 
-
     def p_expr_let(self, p):
         'factor : let let_list in expr'
-        
-        p[0] = LetNode(p[2], p[4], p.slice[1])
 
+        p[0] = LetNode(p[2], p[4], p.slice[1])
 
     def p_expr_case(self, p):
         'factor : case expr of cases_list esac'
-        
-        p[0] = CaseNode(p[2], p[4], p.slice[1])
 
+        p[0] = CaseNode(p[2], p[4], p.slice[1])
 
     def p_expr_if(self, p):
         'factor : if expr then expr else expr fi'
-        
-        p[0] = ConditionalNode(p[2], p[4], p[6], p.slice[1])
 
+        p[0] = ConditionalNode(p[2], p[4], p[6], p.slice[1])
 
     def p_expr_while(self, p):
         'factor : while expr loop expr pool'
-        
-        p[0] = WhileNode(p[2], p[4], p.slice[1])
 
+        p[0] = WhileNode(p[2], p[4], p.slice[1])
 
     def p_atom_num(self, p):
         'atom : num'
-        
+
         p[0] = ConstantNumNode(p.slice[1])
-    
+
     def p_atom_id(self, p):
         'atom : id'
-        
+
         p[0] = VariableNode(p.slice[1])
 
     def p_atom_new(self, p):
@@ -321,11 +299,10 @@ class CoolParser(Parser):
 
     def p_atom_string(self, p):
         'atom : string'
-        
+
         p[0] = ConstantStrNode(p.slice[1])
 
-
-    def p_block(self, p):        
+    def p_block(self, p):
         '''block : expr semi
                  | expr semi block'''
 
@@ -337,10 +314,9 @@ class CoolParser(Parser):
 
         p[0] = [ErrorNode()]
 
-
     def p_func_call(self, p):
         'func_call : id opar args cpar'
-        
+
         p[0] = (p.slice[1], p[3])
 
     def p_func_call_error(self, p):
@@ -349,32 +325,28 @@ class CoolParser(Parser):
 
         p[0] = (ErrorNode(), ErrorNode())
 
-
-    def p_args(self, p):        
+    def p_args(self, p):
         '''args : arg_list
                 | arg_list_empty'''
 
         p[0] = p[1]
-
 
     def p_arg_list(self, p):
         '''arg_list : expr  
                     | expr comma arg_list'''
 
         p[0] = [p[1]] if len(p) == 2 else \
-               [p[1]] + p[3]
-
+            [p[1]] + p[3]
 
     def p_arg_list_error(self, p):
         'arg_list : error arg_list'
-        
+
         p[0] = [ErrorNode()]
 
     def p_arg_list_empty(self, p):
         'arg_list_empty : epsilon'
-        
-        p[0] = []
 
+        p[0] = []
 
     def p_error(self, p):
         self.errors = True
@@ -388,8 +360,7 @@ class CoolParser(Parser):
 
         raise Exception()
 
-    def print_error(self, tok):    
+    def print_error(self, tok):
         error_text = SyntacticError.ERROR % tok.value
         line, column = tok.lineno, tok.column
         print(SyntacticError(error_text, line, column))
-
