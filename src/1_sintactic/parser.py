4ee8e3b511ad9e4a77bf8e3b71450ee3e3704db0
cwjki <cwjki97@gmail.com>
2021-10-15 21:55:56 -0400

    working on the parser

--- src/1_sintactic/parser.py
+++ src/1_sintactic/parser.py
@@ -44,3 +44,30 @@ class CoolParser:
             p[0] = AttrDeclarationNode(p[1], p[3])
         else:
             p[0] = AttrDeclarationNode(p[1], p[3], p[5])
+
+    def p_def_func(self, p):
+        '''def_func : ID LPAREN params RPAREN COLON TYPE LBRACE expr RBRACE'''
+        p[0] = FuncDeclarationNode(p[1], p[3], p[6], p[8])
+
+    def p_params(self, p):
+        '''params : param_list
+                  | param_list_empty'''
+        p[0] = p[1]
+
+    def p_param_list(self, p):
+        '''param_list : param
+                      | param COMMA param_list'''
+        p[0] = [p[1]] if len(p) == 2 else [p[1]] + p[3]
+
+    def p_param_list_empty(self, p):
+        '''para,_list_empty : epsilon'''
+        p[0] = []
+
+    def p_param(self, p):
+        '''param : ID COLON TYPE'''
+        p[0] = (p[1], p[3])
+
+    def p_let_attrs(p):
+        '''let_attrs : def_attr
+                     | def_attr COMMA let_attrs'''
+        p[0] = [p[1]] if len(p) == 2 else [p[1]] + p[3]
