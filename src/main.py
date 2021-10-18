4574f2b377bbd9f7733e5e92def35a27f8841204
JCH97 <jc1997hdez@gmail.com>
2021-10-17 23:30:02 -0400

    (feat): remove comments, run parser tests

--- src/main.py
+++ src/main.py
@@ -6,9 +6,6 @@ from cool.parser.parser import CoolParser
 from cool.utils.errors import SyntacticError
 
 if __name__ == '__main__':
-    # print(f'Argv: {sys.argv} ')
-
-    # path: str = f"{Path.cwd()}/cool/code.cl"  # debug mode
     path: str = f"{Path.cwd()}/cool/code.cl"  # normal mode
 
     _in = sys.argv[1] if len(sys.argv) > 1 else path
@@ -21,19 +18,19 @@ if __name__ == '__main__':
 
     lexer = main(text, _out)
 
-    # tokens = lexer.tokenize(text)
-    #
-    # if len(tokens) == 0:
-    #     error_text = SyntacticError.ERROR % 'EOF'
-    #     print(SyntacticError(error_text, 0, 0))
-    #     raise Exception()
+    tokens = lexer.tokenize(text)
+
+    if len(tokens) == 0:
+        error_text = SyntacticError.ERROR % 'EOF'
+        print(SyntacticError(error_text, 0, 0))
+        raise Exception()
 
     # print(lexer)
 
     # lexer.lexer.lineno = 1
     # lexer.lexer.linestart = 0
 
-    # parser = CoolParser(lexer)
-    # result = parser.parse(text)
+    parser = CoolParser(lexer)
+    result = parser.parse(text)
 
     # print(result)
