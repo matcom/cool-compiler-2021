c05aa02025cefea568ee8a281af853a8337ecb65
AlejandroLabourdette <j.labourdette@estudiantes.matcom.uh.cu>
2021-11-09 17:26:58 -0500

    Added execute_mode in main.py to use automatic tester

--- src/main.py
+++ src/main.py
@@ -4,29 +4,37 @@ from lexer import tokenize
 from parser import parse
 from testers import parser_tester
 
-#########################################################
-# To use this script execute in terminal:               #
-#   python3 main.py module_to_test test_directories     #
-#                                                       #
-# module_to_test can be:                                #
-#   - lexer     - parser                                #
-# test_directories is the dir where tests are, ex:      #
-#   ../tests/lexer                                      #
-#   ../tests/parser                                     #
-#########################################################
+#########################################################################
+# To use this script execute in terminal:                               #
+#   python3 main.py execute_mode module_to_execute program_directory    #
+#                                                                       #
+# execute_mode is to use the tester or just run the program(s), ex:     #
+#   - test      - run                                                   #
+# module_to_execute can be:                                             #
+#   - lexer     - parser                                                #
+# program_directory is the dir where program(s) is(are), ex:            #
+#   ../tests/lexer                                                      #
+#   ../tests/parser                                                     #
+#########################################################################
 
-# programs_directory = sys.argv[2]
-programs_directory = '../tests/parser'
-programs_files = [file for file in os.listdir(programs_directory) if file.endswith('.cl')]
+execute_mode = sys.argv[1]
+module_to_execute = sys.argv[2]
+program_directory = sys.argv[3]
 
-# parser_tester(programs_directory)
+# TEST
+if execute_mode == 'test':
+    if module_to_execute == 'parser':
+        parser_tester(program_directory)
+    else:
+        raise Exception('Not implemented Test Mode for %s' % module_to_execute)
 
-
+# RUN
+programs_files = [file for file in os.listdir(program_directory) if file.endswith('.cl')]
 for program_file in programs_files:
     input('Press enter to analyze ' + program_file)
-    program_route = programs_directory + '/' + program_file
+    program_route = program_directory + '/' + program_file
 
-    # To test lexer
+    # To run lexer
     if sys.argv[1] == 'lexer':
         with open(program_route, 'r', encoding='UTF-8') as f:
             tokens, errors = tokenize(f.read())
@@ -39,13 +47,15 @@ for program_file in programs_files:
             for error in errors:
                 print(error)
 
-    # To test parser
+    # To run parser
     if sys.argv[1] == 'parser':
         with open(program_route, 'r', encoding='UTF-8') as f:
             tree, errors = parse(f.read())
-            print(str(tree))
-
-            print(errors)
+            if len(errors):
+                print(errors)
+            else:
+                pass
+                # print ast
 
     else:
         print('Invalid section to test: ' + sys.argv[1])
