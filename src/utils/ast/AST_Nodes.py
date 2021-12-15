c9f53ac3ca6f62e6e6bdfda378e6a5531a8eda39
Ale <Ale@DESKTOP-KQJNTSU>
2021-12-14 23:08:51 -0800

    ast with tokens files-columns

--- src/utils/ast/AST_Nodes.py
+++ src/utils/ast/AST_Nodes.py
@@ -1,7 +1,11 @@
+from utils.COOL_Lexer import Token
+
+
 class ast_nodes:
 
     class Node:
-        pass
+        line = 0
+        column = 0
 
 
     class ProgramNode(Node):
@@ -15,74 +19,99 @@ class ast_nodes:
 
 
     class ClassDeclarationNode(DeclarationNode):
-        def __init__(self, idx, features, parent=None):
-            self.id = idx
+        def __init__(self, token, features, parent=None):
+            self.id = token.lex
+            self.line = token.line
+            self.column = token.column
             self.parent = parent
             self.features = features
 
     class MethDeclarationNode(DeclarationNode):
-        def __init__(self, idx, params, return_type, body):
-            self.id = idx
+        def __init__(self, token, params, return_type, body):
+            self.id = token.lex
+            self.line = token.line
+            self.column = token.column
             self.params = params
             self.type = return_type
             self.body = body
 
     class AttrDeclarationNode(DeclarationNode):
-        def __init__(self, idx, typex, expr = None):
-            self.id = idx
+        def __init__(self, token, typex, expr = None):
+            self.id = token.lex
+            self.line = token.line
+            self.column = token.column
             self.type = typex
             self.expr = expr
 
     class AssignNode(ExpressionNode):
-        def __init__(self, idx, expr):
-            self.id = idx
+        def __init__(self, token, expr, token_pos):
+            self.id = token.lex
+            self.line = token_pos.line
+            self.column = token_pos.column
             self.expr = expr
 
     class CallNode(ExpressionNode):
-        def __init__(self, idx, args, obj = None, typex = None):
-            self.obj = obj
+        def __init__(self, token, args, obj = None, typex = None):
+            self.obj = obj.lex
             self.type = typex
-            self.id = idx
+            self.id = token.lex
+            self.line = token.line
+            self.column = token.column
             self.args = args
     
     class IfThenElseNode(ExpressionNode):
-        def __init__(self, if_expr, then_expr, else_expr):
+        def __init__(self, if_expr, then_expr, else_expr,token):
             self.if_expr = if_expr
             self.then_expr = then_expr
             self.else_expr = else_expr
+            self.line = token.line
+            self.column = token.column
     
     class WhileNode(ExpressionNode):
-        def __init__(self, conditional_expr, loop_expr):
+        def __init__(self, conditional_expr, loop_expr, token):
             self.conditional_expr = conditional_expr
             self.loop_expr = loop_expr
+            self.line = token.line
+            self.column = token.column
 
     class BlockNode(ExpressionNode):
-        def __init__(self, expr_list):
+        def __init__(self, expr_list, token):
             self.expr_list = expr_list
+            self.line = token.line
+            self.column = token.column
     
     class LetNode(ExpressionNode):
-        def __init__(self, identifiers, in_expr):
-            self.identifiers = identifiers
+        def __init__(self, identifiers, in_expr, token):
+            self.identifiers = [tuple([item.lex if isinstance(item, Token) else item for item in _list]) for _list in identifiers]
             self.in_expr = in_expr
+            self.line = token.line
+            self.column = token.column
 
     class CaseNode(ExpressionNode):
-        def __init__(self, predicate, branches):
+        def __init__(self, predicate, branches, token):
             self.predicate = predicate
-            self.branches = branches
+            self.branches = [tuple([item.lex if isinstance(item, Token) else item for item in _list]) for _list in branches]
+            self.line = token.line
+            self.column = token.column
     
     class NotNode(ExpressionNode):
-        def __init__(self, expr):
+        def __init__(self, expr, token):
             self.expr = expr
+            self.line = token.line
+            self.column = token.column
 
     class AtomicNode(ExpressionNode):
-        def __init__(self, lex):
-            self.lex = lex
+        def __init__(self, token):
+            self.lex = token.lex
+            self.line = token.line
+            self.column = token.column
 
     class BinaryNode(ExpressionNode):
-        def __init__(self, left, right, symbol):
+        def __init__(self, left, right, token):
             self.left = left
             self.right = right
-            self.symbol = symbol
+            self.line = token.line
+            self.column = token.column 
 
 
     class ConstantNumNode(AtomicNode):
