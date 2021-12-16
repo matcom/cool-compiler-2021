d2f0420c6e87f6a8de5ae92e3fa90600895e5ac6
Ale <Ale@DESKTOP-KQJNTSU>
2021-12-16 14:34:40 -0800

    main

---
+++ src/main.py
@@ -0,0 +1,67 @@
+import argparse 
+from utils.COOL_Grammar import build_COOL_Grammar
+from utils.COOL_Lexer import COOL_Lexer
+from utils.parser.COOL_parser import COOL_Parser
+from cmp.evaluation import evaluate_reverse_parse
+from cmp.semantic import Context
+from utils.semantic_check.type_collector import TypeCollector
+from utils.semantic_check.type_builder import TypeBuilder
+from utils.semantic_check.type_checker import TypeChecker
+from utils.semantic_check.type_inference import TypeInference
+from utils.print_code import PrintCode
+
+def main(args):
+    
+    try:
+        with open(args.file, 'r') as file:
+            code = file.read()
+    except:
+        print(f"(0,0) - CompilerError: file {args.file} not found")
+        exit(1)
+
+    G = build_COOL_Grammar()
+
+    # lexer
+    lexer = COOL_Lexer() 
+    tokens = lexer(code)
+
+    if lexer.errors:
+        for error in lexer.errors:
+            print(error)
+        exit(1)
+
+    # parser
+    parser = COOL_Parser(G)
+    derivation, operations = parser(tokens)
+    
+    if parser.error:
+        print(parser.error)
+        exit(1)
+
+    # ast       
+    ast = evaluate_reverse_parse(G, derivation, operations, tokens)
+            
+    context = Context()
+    semantic_errors = []
+
+    collector = TypeCollector(context, semantic_errors)
+    collector.visit(ast)
+
+    builder = TypeBuilder(context, semantic_errors)
+    builder.visit(ast)
+
+    checker = TypeChecker(context, semantic_errors)
+    scope = checker.visit(ast)
+            
+    if semantic_errors:
+        for error in semantic_errors:
+            print(error)                          
+        exit(1)
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description='description')
+    parser.add_argument('-f', '--file', type=str, default='', help='file to read')
+
+    args = parser.parse_args()
+    main(args)
