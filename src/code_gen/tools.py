c71f50b2ffe745dc098a703490d1adfdfc40f6f3
RodroVMS <democlesvanshepars@gmail.com>
2022-01-04 11:36:54 -0500

    Improved LocalVar class

--- src/code_gen/tools.py
+++ src/code_gen/tools.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
 
-from ast.ccil_ast import ExpressionNode
+from asts.ccil_ast import ExpressionNode
 
 
 @dataclass
@@ -9,15 +9,34 @@ class LocalVar:
     ccil_name: str
     boundExpression: ExpressionNode | None
 
+    @property
+    def get_name(self):
+        assert self.ccil_name != ""
+        return self.ccil_name
 
-def make_id(name: str, main_num: int, snd_num: int = -1, thrd_num: int = -1):
+    @staticmethod
+    def new_temporal_var(name: str, boundExpression: ExpressionNode):
+        return LocalVar("", name, boundExpression)
+
+    @staticmethod
+    def new_user_defined_var(
+        name: str, ccil_name: str, boundExpression: ExpressionNode | None = None
+    ):
+        return LocalVar(name, ccil_name, boundExpression)
+
+
+def make_id(
+    name: str, main_num: int, aux_name: str = "", snd_num: int = -1, thrd_num: int = -1
+):
     main_num_str = str(main_num)
     snd_num_str = ""
     thrd_num_str = ""
 
+    if aux_name != "":
+        aux_name = f"_{aux_name}"
     if snd_num != -1:
         snd_num_str = f"_{snd_num}"
     if thrd_num != -1:
         thrd_num_str = f"_{thrd_num}"
 
-    return f"{name}_{main_num_str}{snd_num_str}{thrd_num_str}"
+    return f"{name}{aux_name}_{main_num_str}{snd_num_str}{thrd_num_str}"
