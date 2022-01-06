1c0b7f41bc9bd3e1db66c8449a15a50127e67542
liviton <livi98gp@gmail.com>
2022-01-06 02:01:05 +0100

    cil_ast pag 11 conf hasta vcall

--- src/code_generator/cil_ast.py
+++ src/code_generator/cil_ast.py
@@ -55,6 +55,7 @@ class ArithExpressionNode(ExpressionNode):
         self.left = left
         self.right = right
 
+#Arith
 class PlusNode(ArithExpressionNode):
     pass
 
@@ -75,4 +76,76 @@ class LessEqualNode(ArithExpressionNode):
 
 class EqualNode(ArithExpressionNode):
     pass
+
+#Attr
+class GetAttrNode(ExpressionNode):
+    def __init__(self, dest, instance, attr, static_type):
+        self.local_dest = dest
+        self.instance = instance
+        self.attr = attr
+        self.static_type = static_type
+        
+class SetAttrNode(ExpressionNode):
+    def __init__(self, instance, attr, value, static_type):
+        self.instance = instance
+        self.attr = attr
+        self.value = value
+        self.static_type = static_type
+
+
+#Arrays and Strings
+class GetIndexNode(ExpressionNode):
+    pass
+class SetIndexNode(ExpressionNode):
+    pass
+
+#Memory
+class AllocateNode(ExpressionNode):
+    def __init__(self, t, dest):
+        self.type = t
+        self.local_dest = dest
+        
+class ArrayNode(ExpressionNode):
+    pass
+
+class TypeOfNode(ExpressionNode):
+    def __init__(self, t, dest):
+        self.var = t
+        self.local_dest = dest
+
+#Jumps
+class LabelNode(ExpressionNode):
+    def __init__(self, label):
+        self.label = label
+
+class GoToNode(ExpressionNode):
+    def __init__(self, label):
+        self.label = label
+
+class IfGoTo(ExpressionNode):
+    def __init__(self, condition, label):
+        self.condition = condition
+        self.label = label
+
+#Static Invocation
+class CallNode(ExpressionNode):
+    def __init__(self, dest, func, args, static_type, ret_type):
+        self.local_dest = dest
+        self.function = func
+        self.args = args
+        self.static_type = static_type
+        self.ret_type = ret_type
+
+#Dynamic Invocation
+class VCallNode(ExpressionNode):
+    def __init__(self, instance, dest, func, args, dynamic_type, ret_type):
+        self.instance = instance
+        self.local_dest = dest
+        self.function = func
+        self.args = args
+        self.dynamic_type = dynamic_type
+        self.ret_type = ret_type
+
+
+
 
