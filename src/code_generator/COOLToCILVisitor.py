efdbdf04b20831fe0a255620fd7e94925c95531c
liviton <livi98gp@gmail.com>
2022-01-07 00:52:32 +0100

    COOLToCLIVisitor ProgramNode sin build_in 2.0

--- src/code_generator/COOLToCILVisitor.py
+++ src/code_generator/COOLToCILVisitor.py
@@ -1,6 +1,7 @@
-from cil_ast import *
-import BaseCOOLToCILVisitor
+import cil_ast as cil
+from BaseCOOLToCILVisitor import *
 from utils import visitor
+from parser.ast import *
 
 class COOLToCILVisitor(BaseCOOLToCILVisitor):
     @visitor.on('node')
@@ -12,13 +13,24 @@ class COOLToCILVisitor(BaseCOOLToCILVisitor):
         self.current_function = self.register_function('entry')
         instance = self.define_internal_local()
         result = self.define_internal_local()
-        self.register_instruction(AllocateNode('Main', instance))
-        self.register_instruction(ArgNode(instance))
-        self.register_instruction(CallNode(self.to_function_name('main', 'Main'), result))
-        self.register_instruction(ReturnNode(0))
+        self.register_instruction(cil.AllocateNode('Main', instance))
+        self.register_instruction(cil.ArgNode(instance))
+        self.register_instruction(cil.CallNode(self.to_function_name('main', 'Main'), result))
+        self.register_instruction(cil.ReturnNode(0))
         self.current_function = None
+
        # self.create_built_in()
+
         for declaration, child_scope in zip(node.declarations, scope.children):
             self.visit(declaration, child_scope)
 
-        return ProgramNode(self.dottypes, self.dotdata, self.dotcode)
+        return cil.ProgramNode(self.dottypes, self.dotdata, self.dotcode)
+
+
+
+
+
+
+
+
+
