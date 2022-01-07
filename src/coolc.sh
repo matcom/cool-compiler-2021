f072c449d6e858bc7fa745725c2ef309a05579d0
cwjki <cwjki97@gmail.com>
2022-01-06 21:07:00 -0500

    probar los test

--- src/coolc.sh
+++ src/coolc.sh
@@ -7,5 +7,10 @@ OUTPUT_FILE=${INPUT_FILE:0: -2}mips
 echo "LINEA_CON_NOMBRE_Y_VERSION_DEL_COMPILADOR"        # TODO: Recuerde cambiar estas
 echo "Copyright (c) 2019: Nombre1, Nombre2, Nombre3"    # TODO: líneas a los valores correctos
 
+FILE="__main__.py"
+
 # Llamar al compilador
-echo "Compiling $INPUT_FILE into $OUTPUT_FILE"
+python ${FILE} $INPUT_FILE $OUTPUT_FILE
+
+# Llamar al compilador
+# echo "Compiling $INPUT_FILE into $OUTPUT_FILE"
