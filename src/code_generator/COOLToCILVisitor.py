07d0aca40a0cc704636eb7987404a9eb6799d64f
liviton <livi98gp@gmail.com>
2022-01-11 23:17:30 +0100

    COOLToCLIVisitor ClassDeclarationNode

--- src/code_generator/COOLToCILVisitor.py
+++ src/code_generator/COOLToCILVisitor.py
@@ -32,46 +32,32 @@ class COOLToCILVisitor(BaseCOOLToCILVisitor):
     @visitor.when(ClassDeclarationNode)
     def visit(self, node, scope):
         self.current_type = self.context.get_type(node.id)
-        type_node = self.register_type(node.id)
-        type_node.attributes = [attr.name for attr, _ in self.current_type.all_attributes()]
-        type_node.methods = [(method.name, self.to_function_name(method.name, xtype.name)) for method, xtype in self.current_type.all_methods()]
-          
-        func_declarations = (f for f in node.features if isinstance(f, FuncDeclarationNode))
-        for feature, child_scope in zip(func_declarations, scope.children):
-            self.visit(feature, child_scope)
 
-
-        #init
-        self.current_function = self.register_function(self.init_name(node.id))
-        #allocate
-        instance = self.register_local('instance')
-        self.register_instruction(cil.AllocateNode(node.id, instance))  
-
-        func = self.current_function
-        vtemp = self.define_internal_local()
-
-        #init_attr
-        self.current_function = self.register_function(self.init_name(node.id, attr=True))
-        self.register_param(self.vself)
-        if node.parent != 'Object' and node.parent != 'IO':
-            self.register_instruction(cil.ArgNode(self.vself.name))
-            self.register_instruction(cil.CallNode(self.init_name(node.parent, attr=True), vtemp))
-        attr_declarations = (f for f in node.features if isinstance(f, AttrDeclarationNode))
-        for feature in attr_declarations:
-            self.visit(feature, scope)
+        cil_type_node = self.register_type(self.current_type)
+        cil_type_node.attributes = self.current_type.get_all_attributes()
         
-        self.current_function = func
-        self.register_instruction(cil.ArgNode(instance))
-        self.register_instruction(cil.StaticCallNode(self.init_name(node.id, attr=True), vtemp))
+        if len(cil_type_node.attributes) != 0:
+            constructor = FuncDeclarationNode(node.token, [], node.token, BlockNode([], node.token))
+            func_declarations = [constructor]
+            self.constructors.append(node.id)
+            self.current_type.define_method(self.current_type.name, [], [], self.current_type, node.pos)
+            scopes = [scope] + list(scope.functions.values())
+        else:
+            func_declarations = []
+            scopes = list(scope.functions.values())
 
-        self.register_instruction(cil.ReturnNode(instance))
-        self.current_function = None       
-                
-        self.current_type = None
+        for attr, a_type in cil_type_node.attributes:
+            cil_type_node.attributes.append((attr.name, self.to_attr_name(attr.name, a_type.name)))
+            self.initialize_attr(constructor, attr, scope)            ## add the initialization code in the constructor
+        if cil_type_node.attributes:
+            constructor.body.expr_list.append(SelfNode())
 
+        for method, mtype in self.current_type.all_methods():
+            cil_type_node.methods.append((method.name, self.to_function_name(method.name, mtype.name)))
 
-
-
+        func_declarations += [f for f in node.features if isinstance(f, FuncDeclarationNode)] 
+        for feature, child_scope in zip(func_declarations, scopes):
+            self.visit(feature, child_scope)
 
 
 
