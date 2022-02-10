854fd1e06362c24dfe8b8f3f24e3bf63f127852a
rodrigo-pino <rodrodpino@gmail.com>
2022-02-10 17:29:28 -0500

    Rewrite ccil ast

--- src/asts/ccil_ast.py
+++ src/asts/ccil_ast.py
@@ -1,169 +1,108 @@
-from typing import List, Tuple
-
-from code_gen.tools import LocalVar
+from typing import Dict, List, Tuple
+from __future__ import annotations
 
 
 class Node:
     def __init__(self, node) -> None:
         self.line: int = node.line
         self.col: int = node.col
-        self.type = None
 
     def get_position(self) -> Tuple[int, int]:
-        return self.line, self.col
+        return (self.line, self.col)
 
 
-class ExpressionNode(Node):
-    def __init__(self, node, locals: List[LocalVar] = None) -> None:
+class OperationNode(Node):
+    """
+    Base Class for all operation Nodes
+    """
+
+    def __init__(self, node) -> None:
+        super().__init__(node)
+
+
+class StorageNode(OperationNode):
+    def __init__(self, node, idx: str, operation: ReturnOpNode) -> None:
         """
+        Node used to store the value of operations done.
         Parameters:
-        node <-  Node to set this node positon in the original cool program.
-        locals <- List of local variables needed to be initialized to execute expression.
-        value <- Name of the local variable where the expresion final value is going to be stored.
+        node <- Node to maintain knowledge of colummn and position in the original code.
+        idx <- Id of this node.
+        opeartion <- The operation this node is storing.
         """
         super().__init__(node)
-        if locals is None:
-            locals = []
-
-        self.locals = locals
-
-    @property
-    def value(self):
-        return self.locals[-1]
+        self.id = idx
+        self.operation = operation
 
 
-class AtomicNode(ExpressionNode):
-    pass
-
-
-class StringNode(AtomicNode):
-    pass
-
-
-class IntNode(AtomicNode):
-    pass
-
-
-class VariableNode(ExpressionNode):
-    pass
-
-
-class VarDeclarationNode(ExpressionNode):
-    def __init__(self, expr: ExpressionNode, node, locals: List = None) -> None:
-        super().__init__(node, locals=locals)
-        self.expression = expr
-
-
-class LetNode(ExpressionNode):
-    pass
-
-
-class LoopNode(ExpressionNode):
-    pass
-
-
-class CaseOptionNode(ExpressionNode):
-    pass
-
-
-class CaseNode(ExpressionNode):
-    def __init__(
-        self,
-        case_expr: ExpressionNode,
-        options: List[CaseOptionNode],
-        node,
-        locals: List = None,
-    ) -> None:
-        super().__init__(node, locals=locals)
-        self.case_expr = case_expr
-        self.options = options
-
-
-class ConditionalNode(ExpressionNode):
-    def __init__(
-        self,
-        condition: ExpressionNode,
-        then_node: ExpressionNode,
-        else_node: ExpressionNode,
-        node,
-        locals: List = None,
-    ) -> None:
-        super().__init__(node, locals=locals)
-        self.condition = condition
-        self.then_node = then_node
-        self.else_node = else_node
-
-
-class BlocksNode(ExpressionNode):
-    def __init__(
-        self,
-        expression_list: List[ExpressionNode],
-        node,
-        locals: List = None,
-    ) -> None:
-        super().__init__(node, locals=locals)
-        self.expression_list = expression_list
-
-
-class LetVarDeclarationNode(ExpressionNode):
-    def __init__(self, expression: ExpressionNode, node, locals=None):
-        super().__init__(self, node, locals=locals)
-        self.id = node.id
-        self.type = node.type
-        self.expression = expression
-
-
-class DeclarationNode(Node):
-    pass
-
-
-class AttrDeclarationNode(DeclarationNode):
-    def __init__(self, expression: ExpressionNode | None, node) -> None:
+class ReturnOpNode(OperationNode):
+    def __init__(self, node) -> None:
         super().__init__(node)
-        self.id = node.id
-        self.type = node.type
-        self.expression = expression
-        self.value = expression.value if expression is not None else None
 
 
-class MethodDeclarationNode(DeclarationNode):
-    def __init__(
-        self,
-        idx: str,
-        params: List[VarDeclarationNode],
-        body: ExpressionNode,
-        node,
-    ) -> None:
+class BinaryOpNode(ReturnOpNode):
+    def __init__(self, node, left: AtomOpNode, right: AtomOpNode) -> None:
+        """
+        Node that represents all binary operation
+        Parameters:
+        left <- Left atomic node.
+        right <- Right atomic node.
+        """
         super().__init__(node)
-        self.idx: str = idx
-        self.params: List[VarDeclarationNode] = params
-        self.body: ExpressionNode = body
-        self.value = body.value
+        self.left = left
+        self.right = right
 
 
-class ClassDeclarationNode(Node):
-    def __init__(
-        self,
-        attributes: List[AttrDeclarationNode],
-        methods: List[MethodDeclarationNode],
-        feature_info: List,
-        node,
-    ) -> None:
+class AtomOpNode(ReturnOpNode):
+    def __init__(self, node) -> None:
+        """
+        AtomNode represents all single value nodes, like ids and constants
+        """
         super().__init__(node)
-        self.idx: str = node.id
-        self.parent: str = node.parent
-        self.attributes: List[AttrDeclarationNode] = attributes
-        self.methods: List[MethodDeclarationNode] = methods
-        self.feature_info: List = feature_info
 
 
-class ProgramNode(Node):
-    def __init__(
-        self,
-        types: List[ClassDeclarationNode],
-        data: List[str],
-        node,
-    ) -> None:
+class IdNode(AtomOpNode):
+    def __init__(self, node, idx: str) -> None:
         super().__init__(node)
-        self.types = types
-        self.data = data
+        self.id = idx
+
+
+class ConstantNode(AtomOpNode):
+    def __init__(self, node) -> None:
+        super().__init__(node)
+
+
+class FlowControlNode(OperationNode):
+    """
+    Base class for all flow control operations like If, Label, goto, etc...
+    """
+
+    def __init__(self, node) -> None:
+        super().__init__(node)
+
+
+class IfNode(FlowControlNode):
+    def __init__(self, node, eval_value: StorageNode, target: LabelNode) -> None:
+        super().__init__(node)
+        self.eval_value = eval_value
+        self.target = target
+
+
+class IfFalseNode(IfNode):
+    def __init__(self, node, eval_value: StorageNode, target: LabelNode) -> None:
+        super().__init__(node, eval_value, target)
+
+
+class GoToNode(FlowControlNode):
+    def __init__(self, node, target: LabelNode) -> None:
+        super().__init__(node)
+        self.target = target
+
+
+class LabelNode(FlowControlNode):
+    def __init__(self, node, idx: str) -> None:
+        super().__init__(node)
+        self.id = idx
+
+
+def create_assignation(node, idx: str, target: str):
+    return StorageNode(node, idx, IdNode(node, target))
