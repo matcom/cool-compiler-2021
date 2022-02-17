67e780a30cfc6c69875f37ceab6c616ba47ae356
Luis E. Dalmau <lukedalmau@gmail.com>
2022-02-17 13:53:45 -0500

    comment argument = None

--- src/cool_cmp/cool/ast/cil_ast.py
+++ src/cool_cmp/cool/ast/cil_ast.py
@@ -9,43 +9,43 @@ from cmp.cil import *
 # META INSTRUCTIONS
 
 class ObjectAbortNode(InstructionNode):
-    def __init__(self,row,column,comment):
+    def __init__(self,row,column,comment=None):
         super().__init__(row,column,comment)
 
 class ObjectCopyNode(InstructionNode):
-    def __init__(self,row,column,comment):
+    def __init__(self,row,column,comment=None):
         super().__init__(row,column,comment)
         
 class ObjectTypeNameNode(InstructionNode):
-    def __init__(self,row,column,comment):
+    def __init__(self,row,column,comment=None):
         super().__init__(row,column,comment)
 
 class StringLengthNode(InstructionNode):
-    def __init__(self,row,column,comment):
+    def __init__(self,row,column,comment=None):
         super().__init__(row,column,comment)
         
 class StringConcatNode(InstructionNode):
-    def __init__(self,row,column,comment):
+    def __init__(self,row,column,comment=None):
         super().__init__(row,column,comment)
 
 class StringSubstringNode(InstructionNode):
-    def __init__(self,row,column,comment):
+    def __init__(self,row,column,comment=None):
         super().__init__(row,column,comment)
 
 class IOOutStringNode(InstructionNode):
-    def __init__(self,row,column,comment):
+    def __init__(self,row,column,comment=None):
         super().__init__(row,column,comment)
         
 class IOOutIntNode(InstructionNode):
-    def __init__(self,row,column,comment):
+    def __init__(self,row,column,comment=None):
         super().__init__(row,column,comment)
         
 class IOInStringNode(InstructionNode):
-    def __init__(self,row,column,comment):
+    def __init__(self,row,column,comment=None):
         super().__init__(row,column,comment)
         
 class IOInIntNode(InstructionNode):
-    def __init__(self,row,column,comment):
+    def __init__(self,row,column,comment=None):
         super().__init__(row,column,comment)
 
 ################################################################################
@@ -60,14 +60,14 @@ class IOInIntNode(InstructionNode):
 # OTHER INSTRUCTIONS
 
 class GetFatherNode(InstructionNode):
-    def __init__(self, dest, variable,row, column, comment) -> None:
+    def __init__(self, dest, variable,row, column, comment=None)-> None:
         super().__init__(row, column, comment)
         self.dest = dest
         self.variable = variable
 
 
 class VoidNode(InstructionNode):
-    def __init__(self, dest, row, column, comment) -> None:
+    def __init__(self, dest, row, column, comment=None)-> None:
         super().__init__(row, column, comment)
         self.dest = dest
 
@@ -83,25 +83,25 @@ class VoidNode(InstructionNode):
 # ARITHMETIC INSTRUCTIONS
 
 class UnaryArithmeticNode(InstructionNode):
-    def __init__(self, dest, value, row, column, comment) -> None:
+    def __init__(self, dest, value, row, column, comment=None)-> None:
         super().__init__(row, column, comment)
         self.dest = dest
         self.value = value
 
 class NotNode(UnaryArithmeticNode):
-    def __init__(self, row, column, comment) -> None:
+    def __init__(self, row, column, comment=None)-> None:
         super().__init__(row, column, comment)
 
 class EqualNode(ArithmeticNode):
-    def __init__(self, row, column, comment) -> None:
+    def __init__(self, row, column, comment=None)-> None:
         super().__init__(row, column, comment)
 
 class GreaterNode(ArithmeticNode):
-    def __init__(self, row, column, comment) -> None:
+    def __init__(self, row, column, comment=None)-> None:
         super().__init__(row, column, comment)
 
 class LesserNode(ArithmeticNode):
-    def __init__(self, row, column, comment) -> None:
+    def __init__(self, row, column, comment=None)-> None:
         super().__init__(row, column, comment)
 
 ################################################################################
