73faaefaf2e66d8218defaa1741697f309b8146b
regnod <richard.garcia@estudiantes.matcom.uh.cu>
2022-02-23 15:55:58 -0500

    main test

--- src/main.py
+++ src/main.py
@@ -6,23 +6,23 @@ from sys import argv
 
 
 # with open('/home/yumenio/Documents/cmp/cool-compiler-2021/cool-compiler-2021/tests/semantic/self4.cl') as f:
-with open('./tests/codegen/hello_world.cl') as f:
-    program =  f.read()
+# with open('./tests/codegen/hello_world.cl') as f:
+#     program =  f.read()
 
-coolLexer = lexer.CoolLexer()
-coolLexer.build()
-tokens = coolLexer.input(program)
-# for error in coolLexer.errors:
-#     print(error)
-Pipeline(program, lexer.CoolLexer(),parser.CoolParser(), False)
-coolParser = parser.CoolParser()
+# coolLexer = lexer.CoolLexer()
+# coolLexer.build()
+# tokens = coolLexer.input(program)
+# # for error in coolLexer.errors:
+# #     print(error)
+# Pipeline(program, lexer.CoolLexer(),parser.CoolParser(), False)
+# coolParser = parser.CoolParser()
 
-ast = coolParser.parse(lexer.CoolLexer(), program)
-for lexing_error in coolParser.lexer.errors:
-    print(lexing_error)
-for parsing_error in coolParser.errors:
-    print(parsing_error)
-a = 0
+# ast = coolParser.parse(lexer.CoolLexer(), program)
+# for lexing_error in coolParser.lexer.errors:
+#     print(lexing_error)
+# for parsing_error in coolParser.errors:
+#     print(parsing_error)
+# a = 0
 
 if __name__ == '__main__':
     # with open('examles/hello_world.cl', 'r') as fd:
