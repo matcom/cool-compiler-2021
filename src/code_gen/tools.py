236eebc9eccc7d22ff3d6e5043e9d66a6eda2907
rodrigo-pino <rodrodpino@gmail.com>
2022-02-23 10:44:00 -0500

    Remove helpers which turned out not to be so helpfuls

--- src/code_gen/tools.py
+++ src/code_gen/tools.py
@@ -3,20 +3,3 @@ from dataclasses import dataclass
 from asts.ccil_ast import ExpressionNode
 from typing import List
 from __future__ import annotations
-
-
-@dataclass(frozen=True)
-class Class:
-    attributes: List[Attribute]
-    methods: List[Method]
-
-
-@dataclass(frozen=True)
-class Attribute:
-    id: str
-    type: str
-
-
-@dataclass(frozen=True)
-class Method:
-    id: str
