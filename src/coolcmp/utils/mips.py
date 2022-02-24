a495f9da1b239d37c4b6a44a20c8cca413e69af1
samueldsr8 <samueldsr8@gmail.com>
2022-02-24 03:12:26 -0500

    feat: Add methods for get offset address on Types

--- src/coolcmp/utils/mips.py
+++ src/coolcmp/utils/mips.py
@@ -1,11 +1,27 @@
 from __future__ import annotations
-from typing import List, Union
+from typing import List, Tuple, Union
 
-from coolcmp.utils.registers import Register, SP, DW
+from coolcmp.utils.registers import FP, Register, SP, DW
 
 
 class Node:
     pass
+
+
+class Type:
+    def __init__(self, label, address, attrs, methods, index, default=None):
+        self.label = label
+        self.address = address
+        self.attrs = attrs
+        self.default_attrs = dict(default) if default is not None else {}
+        self.methods = methods
+        self.index = index
+
+    def length(self):
+        return len(self.attrs)
+
+    def __str__(self):
+        return f"{self.label}-{self.address}-{self.attrs}-{self.default_attrs}-{self.methods}-{self.index}"
 
 
 class ProgramNode(Node):
@@ -26,6 +42,24 @@ class FunctionNode(Node):
         self.params = params
         self.local_vars = local_vars
         self.instructions = []
+
+    def param_address(self, name: str) -> Tuple[Register, int]:
+        index = self.params.index(name)
+        params_amount = len(self.params)
+        offset = (params_amount - 1 - index) * DW
+        return (FP, offset)
+
+    def local_address(self, name: str):
+        index = self.local_vars.index(name)
+        locals_amount = len(self.local_vars)
+        offset = (locals_amount + 2 - index) * DW
+        return (FP, offset)
+
+    def variable_address(self, name: str):
+        try:
+            return self.param_address(name)
+        except ValueError:
+            return self.local_address(name)
 
 
 class InstructionNode(Node):
@@ -49,6 +83,7 @@ class SWNode(InstructionNode):
     store word | sw $1, 100($2) | Memory[$2 + 100] = $1
     Copy from register to memory.
     """
+
     def __init__(self, dest: int, offset: int, src: Register):
         self.dest = dest
         self.offset = offset
@@ -60,6 +95,7 @@ class LWNode(InstructionNode):
     load word | lw $1, 100($2) | $1 = Memory[$2 + 100]
     Copy from memory to register.
     """
+
     def __init__(self, dest: Register, offset: int, src: int):
         self.dest = dest
         self.offset = offset
@@ -71,6 +107,7 @@ class LINode(InstructionNode):
     load immediate | li $1, 100 | $1 = 100
     Loads immediate value into register.
     """
+
     def __init__(self, reg: Register, value: int):
         self.reg = reg
         self.value = value
@@ -82,6 +119,7 @@ class JALNode(InstructionNode):
     Use when making procedure call.
     This saves the return address in $ra.
     """
+
     def __init__(self, dest: int):
         self.dest = dest
 
@@ -91,6 +129,7 @@ class LANode(InstructionNode):
     load address | la $1, label | $1 = Address of label
     Loads computed address of label (not its contents) into register.
     """
+
     def __init__(self, reg: Register, label: str):
         self.reg = reg
         self.label = label
@@ -100,6 +139,7 @@ class ADDNode(InstructionNode):
     """
     add | add $1, $2, $3 | $1 = $2 + $3
     """
+
     def __init__(self, dest: Register, src1: Register | int, src2: Register | int):
         self.dest = dest
         self.src1 = src1
@@ -111,6 +151,7 @@ class ADDINode(InstructionNode):
     add immediate | addi $1, $2, 100 | $1 = $2 + 100
     "Immediate" means a constant number.
     """
+
     def __init__(self, dest: Register, src: Register | int, isrc: Register | int):
         self.dest = dest
         self.src = src
@@ -122,6 +163,7 @@ class JRNode(InstructionNode):
     jump register | jr $1 | go to address stored in $1
     For switch, procedure return.
     """
+
     def __init__(self, dest: Register):
         self.dest = dest
 
@@ -135,6 +177,7 @@ class PrintIntNode(SysCallNode):
     print_int | $a0 = integer to be printed | code in v0 = 1
     Print integer number (32 bit).
     """
+
     pass
 
 
@@ -143,23 +186,8 @@ class PrintStringNode(SysCallNode):
     print_string | $a0 = address of string in memory | code in v0 = 4
     Print null-terminated character string.
     """
+
     pass
-
-
-class Type:
-    def __init__(self, label, address, attrs, methods, index, default=None):
-        self.label = label
-        self.address = address
-        self.attrs = attrs
-        self.default_attrs = dict(default) if default is not None else {}
-        self.methods = methods
-        self.index = index
-
-    def length(self):
-        return len(self.attrs)
-
-    def __str__(self):
-        return f"{self.label}-{self.address}-{self.attrs}-{self.default_attrs}-{self.methods}-{self.index}"
 
 
 def push_register_instructions(reg_name: str) -> List[InstructionNode]:
