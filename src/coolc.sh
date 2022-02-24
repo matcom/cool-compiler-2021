bd837098a136b9a35e6dc4446bd0da3c9fc074fd
Carlos <carlozalejandro98@gmail.com>
2022-02-24 18:01:06 -0300

    reset testing

--- src/coolc.sh
+++ src/coolc.sh
@@ -1,8 +1,11 @@
+# Incluya aquí las instrucciones necesarias para ejecutar su compilador
+# Incluya aquí las instrucciones necesarias para ejecutar su compilador
+
 INPUT_FILE=$1
 OUTPUT_FILE=${INPUT_FILE:0: -2}mips
 
-echo "Cool Compiler v1.0"
-echo "Copyright (c) 2022: Luis Lara , Carlos Arrieta"
+echo "Cool Compiler v0.1"
+echo "Copyright (c) 2022: Luis Lara, Carlos Arrieta"
 
 # Llamar al compilador
-python3 main.py ${INPUT_FILE}
+python3 main.py ${INPUT_FILE} 
