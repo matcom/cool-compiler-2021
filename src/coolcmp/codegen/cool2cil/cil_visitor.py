34ec4f97ac3f6e8f5cd9c200a157754ba3aac58d
Enmanuel Verdesia Suarez <everdesiasuarez@gmail.com>
2022-02-24 15:06:05 -0500

    feat: Add more basic functions

--- src/coolcmp/codegen/cool2cil/cil_visitor.py
+++
@@ -1,9 +0,0 @@
-class CILVisitor:
-
-    def __init__(self):
-        self.function_id: int = -1
-
-    @property
-    def next_function_id(self) -> int:
-        self.function_id += 1
-        return self.function_id
