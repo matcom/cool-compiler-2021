16e39515d6118d678aea3c748c6f515ba5a1bce7
ariel.plasencia <arielplasencia00@gmail.com>
2022-02-24 22:44:55 -0500

    remove carlitos

--- src/coolc.sh
+++ src/coolc.sh
@@ -2,8 +2,8 @@
 INPUT_FILE=$1
 OUTPUT_FILE=${INPUT_FILE:0: -2}mips
 
-echo "COOL Compiler v2.0.0"
-echo "Copyright (©) 2021: Juan José López, Juan Carlos Esquivel, Carlos Martínez, Ariel Plasencia"
+echo "COOL Compiler v3.0.0"
+echo "Copyright (©) 2021: Juan José López, Juan Carlos Esquivel, Ariel Plasencia"
 
 # echo "Compiling $INPUT_FILE into $OUTPUT_FILE"
 python3 main.py $INPUT_FILE $OUTPUT_FILE
