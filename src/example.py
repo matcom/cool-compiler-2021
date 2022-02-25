95af3262b115a4d00fa41d491209b779fc507bd5
samueldsr8 <samueldsr8@gmail.com>
2022-02-24 19:55:40 -0500

    Upload test examples

---
+++ src/example.py
@@ -0,0 +1,29 @@
+from coolcmp.codegen.cil2mips.mips_formatter import MIPSFormatter
+from coolcmp.lexing_parsing.lexer import errors as lexer_errors
+from coolcmp.lexing_parsing.parser import parser, errors as parser_errors
+from coolcmp.semantics import check_semantics
+from coolcmp.codegen.cool2cil import build_cil
+
+from coolcmp.codegen.cil2mips import build_mips
+from coolcmp.utils.ast_formatter import ASTFormatter
+from coolcmp.utils.cil_formatter import CILFormatter
+
+from test_cil2mips import hello_world, allocate
+
+
+def main():
+    cil_ast = allocate
+
+    mips_ast = build_mips(cil_ast, None, None)
+
+    mips_str = MIPSFormatter().visit(mips_ast)
+
+    print("Mips code:")
+    print(mips_str)
+
+    with open("./code.mips", "w") as fd:
+        fd.write(mips_str)
+
+
+if __name__ == "__main__":
+    main()
--- src/test_cil2mips.py
+++ src/test_cil2mips.py
@@ -2,25 +2,69 @@ from coolcmp.utils import cil
 from coolcmp.utils import CILFormatter
 
 
-test_cil = cil.ProgramNode(
+hello_world = cil.ProgramNode(
     dot_types=[],
     dot_data=[
-        cil.DataNode('msg', '"Hello World!\n"'),
+        cil.DataNode("hello_message", '"Hello\n"'),
+        cil.DataNode("world_message", '"World!\n"'),
     ],
     dot_code=[
         cil.FunctionNode(
-            name='main',
+            name="main",
             params=[],
             local_vars=[
-                cil.LocalNode('x'),
+                cil.LocalNode("x"),
+                cil.LocalNode("y"),
+                cil.LocalNode("z"),
             ],
             instructions=[
-                cil.LoadNode('x', 'msg'),
-                cil.PrintStringNode('x'),
+                cil.LoadNode("x", "hello_message"),
+                cil.PrintStringNode("x"),
+                cil.LoadNode("y", "world_message"),
+                cil.PrintStringNode("y"),
+                cil.LoadNode("z", "hello_message"),
+                cil.PrintStringNode("z"),
                 cil.ReturnNode(0),
-            ]
+            ],
         )
-    ]
+    ],
 )
 
-print(CILFormatter().visit(test_cil))
+allocate = cil.ProgramNode(
+    dot_types=[cil.TypeNode("Int", ["value"], [], [])],
+    dot_data=[
+        cil.DataNode("the_value_is", '"The value is\n"'),
+    ],
+    dot_code=[
+        cil.FunctionNode(
+            name="main",
+            params=[],
+            local_vars=[cil.LocalNode("a"), cil.LocalNode("msg")],
+            instructions=[
+                cil.AllocateNode("Int", "a"),
+                cil.LoadNode("msg", "the_value_is"),
+                cil.PrintStringNode("msg"),
+                cil.PrintIntNode("a"),
+                cil.ReturnNode(0),
+            ],
+        )
+    ],
+)
+
+test_cil2 = cil.ProgramNode(
+    dot_types=[],
+    dot_data=[cil.DataNode("msg", '"Hola mundo\n"')],
+    dot_code=[
+        cil.FunctionNode(
+            name="main",
+            params=[],
+            local_vars=[cil.LocalNode("x"), cil.LocalNode("y"), cil.LocalNode("sum")],
+            instructions=[
+                cil.LoadNode("x", 1),
+                cil.LoadNode("y", 2),
+                cil.PrintStringNode("x"),
+                cil.ReturnNode(0),
+            ],
+        )
+    ],
+)
