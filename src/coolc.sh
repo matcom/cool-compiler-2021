30d4570d6c5a68dcea2869ad647d54c82b6f4b07
Carmen Cabrera <kr1000a@gmail.com>
2022-03-03 07:55:39 -0500

    changing default goal in makefile

--- src/coolc.sh
+++ src/coolc.sh
@@ -4,4 +4,4 @@ echo "codersUP - COOLCompilerv0.0.1"
 echo "Copyright (c) 2022: Carmen Irene Cabrera Rodríguez, David Guaty Domínguez, Enrique Martínez González"
 
 # Run the compiler
-python3 main.py -f $INPUT_FILE
+python3 main.py -f "$INPUT_FILE"
