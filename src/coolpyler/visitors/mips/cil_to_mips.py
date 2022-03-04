55f0b242d1aec13edc8e17312284286e473c6bea
Rodrigo <rodrigo.garcia@estudiantes.matcom.uh.cu>
2022-03-04 00:33:19 -0500

    recover concat

--- src/coolpyler/visitors/mips/cil_to_mips.py
+++ src/coolpyler/visitors/mips/cil_to_mips.py
@@ -915,6 +915,91 @@ class CilToMIPS:
         self.memory_manager.clean()
         return instructions
 
+    @visitor.when(cil.ConcatNode)
+    def visit(self, node: cil.ConcatNode):
+        instructions = []
+        self.memory_manager.save()
+
+        loop1 = f"loop{self.get_loop_count()}"
+        exit1 = f"exit{self.get_exit_count()}"
+        loop2 = f"loop{self.get_loop_count()}"
+        exit2 = f"exit{self.get_exit_count()}"
+
+        reg1 = self.memory_manager.get_unused_register()
+        reg2 = self.memory_manager.get_unused_register()
+
+        lenght_dir = self.search_mem(node.dest_lenght)
+
+        instructions.append(
+            mips.LoadWordNode(
+                reg1,
+                mips.MemoryAddressRegisterNode(FP_REG, lenght_dir),
+                f"Concat two Strings",
+            )
+        )
+
+        instructions.append(mips.AddiNode(reg1, reg1, 1))
+        instructions.append(mips.LoadInmediateNode(V0_REG, 9))
+        instructions.append(mips.MoveNode(ARG_REGISTERS[0], reg1))
+        instructions.append(mips.SyscallNode())
+
+        instructions.append(
+            mips.LoadAddressNode(reg2, mips.MemoryAddressRegisterNode(V0_REG, 0))
+        )
+
+        string1_dir = self.search_mem(node.string1)
+        instructions.append(
+            mips.LoadWordNode(
+                ARG_REGISTERS[1], mips.MemoryAddressRegisterNode(FP_REG, string1_dir)
+            )
+        )
+        string2_dir = self.search_mem(node.string2)
+        instructions.append(
+            mips.LoadWordNode(
+                ARG_REGISTERS[2], mips.MemoryAddressRegisterNode(FP_REG, string2_dir)
+            )
+        )
+
+        instructions.append(mips.LabelInstructionNode(loop1))
+        instructions.append(
+            mips.LoadByteNode(reg1, mips.MemoryAddressRegisterNode(ARG_REGISTERS[1], 0))
+        )
+
+        instructions.append(mips.BeqzNode(reg1, exit1))
+        instructions.append(
+            mips.StoreByteNode(reg1, mips.MemoryAddressRegisterNode(V0_REG, 0))
+        )
+
+        instructions.append(mips.AddiNode(V0_REG, V0_REG, 1))
+        instructions.append(mips.AddiNode(ARG_REGISTERS[1], ARG_REGISTERS[1], 1))
+        instructions.append(mips.JumpNode(loop1))
+        instructions.append(mips.LabelInstructionNode(exit1))
+
+        instructions.append(mips.LabelInstructionNode(loop2))
+        instructions.append(
+            mips.LoadByteNode(reg1, mips.MemoryAddressRegisterNode(ARG_REGISTERS[2], 0))
+        )
+        instructions.append(
+            mips.StoreByteNode(reg1, mips.MemoryAddressRegisterNode(V0_REG, 0))
+        )
+        instructions.append(mips.BeqzNode(reg1, exit2))
+        instructions.append(mips.AddiNode(V0_REG, V0_REG, 1))
+        instructions.append(mips.AddiNode(ARG_REGISTERS[2], ARG_REGISTERS[2], 1))
+        instructions.append(mips.JumpNode(loop2))
+        instructions.append(mips.LabelInstructionNode(exit2))
+
+        dest_dir = self.search_mem(node.dest)
+        instructions.append(
+            mips.StoreWordNode(
+                reg2,
+                mips.MemoryAddressRegisterNode(FP_REG, dest_dir),
+                f"Save concated Strings",
+            )
+        )
+
+        self.memory_manager.clean()
+        return instructions
+
     @visitor.when(cil.PrefixNode)
     def visit(self, node: cil.PrefixNode):
         instructions = []
