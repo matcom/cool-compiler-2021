05b2287cc8df36ace87defbde239996b50792206
Jorge Mederos <jmederosalvarado@gmail.com>
2022-03-04 23:04:57 -0500

    testscript

--- test.sh
+++ test.sh
@@ -2,4 +2,4 @@
 
 CURDIR=$(dirname $0)
 
-$CURDIR/src/coolc.sh "$CURDIR/tests/$1/$2.cl" && spim -file "$CURDIR/tests/$1/$2.mips"
+$CURDIR/src/coolc.sh "$CURDIR/tests/$1/$2.cl" && spim -file "$CURDIR/tests/$1/$2.mips" < "$CURDIR/tests/$1/${2}_input.txt"
