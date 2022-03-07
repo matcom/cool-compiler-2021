acea9c330e77040e39f91e8392c5b37c5b892d21
Thalia Blanco <73006545+liablanco98@users.noreply.github.com>
2022-03-07 02:36:37 -0500

    Adapting to py 3.7
    Has lexer & parser of regex and cool, use main_

---
+++ ast_regex.py
@@ -0,0 +1,201 @@
+from enum import Enum, auto
+from automaton_class import Automaton
+from state_class import State
+from token_class import Token
+from typing import Union
+import string
+from abc import ABC, abstractmethod
+from dataclasses import dataclass
+from itertools import chain
+
+INVALID = {"", ""}
+SPECIAL = {"”","“","∗","\0","≤"}
+ALPHABET = set(chain.from_iterable([string.printable,SPECIAL])) - INVALID
+DIGIT = set(iter(string.digits))
+NONDIGIT = ALPHABET - DIGIT
+
+def any_escaper(left: Union[Token ,None], right: Union[Token,None]):
+    if left.lexeme == "." and right.lexeme == "." or left.lexeme =="\\" and right.lexeme != ".":
+        lex = right.lexeme
+        res= DIGIT if lex=="d" else NONDIGIT if lex=="D" else ALPHABET if lex=="." else lex
+        return res
+    return {right.lexeme}
+
+
+def transitions_appender(chars):
+    res = Automaton()
+    state = State()
+    final = State(final=True)
+    res.add_state(state)
+    res.add_state(final)
+    for char in chars:
+        state[char] = final
+    return res
+
+
+class TOKEN_TYPE(Enum):
+    ALT = auto()
+    STAR = auto()
+    PLUS = auto()
+    MINUS = auto()
+    ASK = auto()
+    ESC = auto()
+    DOT = auto()
+    ACC = auto()
+    LPAREN = auto()
+    RPAREN = auto()
+    LBRACE = auto()
+    RBRACE = auto()
+    CHAR=auto()
+
+
+@dataclass
+class Node(ABC):
+    @abstractmethod
+    def shift(self):
+        pass
+
+
+class BinaryNode(Node, ABC):
+    def __init__(self,left:Union[Node,Token], right:Union[Node,Token]):
+        self.left:Union[Node,Token]=left
+        self.right:Union[Node,Token]=right
+
+    def __str__(self) -> str:
+        return f"LEFT: {self.left} \ RIGHT: {self.right}"    
+
+
+class UnaryNode(Node, ABC):
+    def __init__(self,left:Union[Node,Token]):
+        self.left=left
+
+    def __str__(self) -> str:
+        return f"LEFT: {self.left}"
+
+# A | B
+class UnionNode(BinaryNode):
+    def __init__(self, left:Union[Node,Token], right: Union[Node,Token]):
+        super().__init__(left, right)
+
+    def shift(self):
+        left :Automaton= self.left.shift()
+        right:Automaton = self.right.shift()
+        return left | right
+
+# A + B
+class ConcatenationNode(BinaryNode):
+    def __init__(self, left: Union[Node,Token], right: Union[Node,Token]):
+        super().__init__(left, right)
+
+    def shift(self):
+        left :Automaton= self.left.shift()
+        right:Automaton = self.right.shift()
+        return left + right
+
+#s* 0-many times
+class ClousureStarNode(UnaryNode):
+    def __init__(self, left: Union[Node,Token]):
+        super().__init__(left)
+
+    def shift(self):
+        left:Automaton = self.left.shift()
+        res = left.upd_stars()
+        return res.upd_finals()
+
+#s+ at least one time
+class ClousurePlusNode(UnaryNode):
+    def __init__(self, left: Union[Node,Token]):
+        super().__init__(left)
+
+    def shift(self):
+        left: Automaton = self.left.shift()
+        left0 = left.copy()
+        left1 = left.copy()
+        r = left1.upd_stars()
+        res = left0 + r
+        return res.upd_finals()
+
+
+#s? 0-1 times
+class ClousureMayNode(UnaryNode):
+    def __init__(self, left: Union[Node,Token]):
+        super().__init__(left)
+
+    def shift(self):
+        left:Automaton = self.left.shift()
+        return left.upd_stars()
+
+#(c)
+class GrouperNode(UnaryNode):
+    def __init__(self, left: Union[Node,Token]):
+        super().__init__(left)
+
+    def shift(self):
+        return self.left.shift()
+
+#[ElemSet]
+class SetterNode(UnaryNode):
+    def __init__(self, left: Union[Node,Token]):
+        super().__init__(left)
+
+    def shift(self):
+        rang=self.left.shift()
+        return transitions_appender(rang)
+
+#[^ ElemSet]
+class NoSetterNode(UnaryNode):
+    def __init__(self, left: Union[Node,Token]):
+        super().__init__(left)
+
+    def shift(self):
+        alphabet = ALPHABET - self.left.shift()
+        return transitions_appender(alphabet)
+
+# . or \
+class AnyEscaperNode(BinaryNode):
+    def __init__(self, left: Union[Node,Token], right: Union[Node,Token]):
+        super().__init__(left, right)
+
+    def shift(self):
+        return transitions_appender(any_escaper(self.left,self.right))
+
+# May be a . \ or token 
+class ElemSetComNode(BinaryNode):
+    def __init__(self, left: Union[Node,Token], right: Union[Node,Token]):
+        super().__init__(left, right)
+
+    def shift(self):
+        left=any_escaper(self.left.left,self.left.right) if isinstance(self.left , AnyEscaperNode) else None
+        left={self.left.lexeme} if isinstance(self.left , Token) else left
+
+        right=any_escaper(self.right.left,self.right.right) if isinstance(self.right , AnyEscaperNode) else None
+        right={self.right.lexeme} if isinstance(self.right , Token) else right
+
+        f_left = left if left else self.left.shift()
+        f_right = right if right else self.right.shift()
+        return set(chain(f_left, f_right))
+
+#[a-c] abc
+class RangeNode(BinaryNode):
+    def __init__(self, left: Union[Node,Token], right: Union[Node,Token]):
+        super().__init__(left, right)
+
+    def shift(self):
+        left = self.left.lexeme
+        right = self.right.lexeme
+        if ord(left) > ord(right):
+            raise Exception("Invalid Input Range Order")
+        
+        chars=set()    
+        for x in range(ord(left),ord(right)+1):
+            chars.add(chr(x))
+        return chars
+
+# a Token Char needs to be converted into a automaton to have a way to be shifted 
+class CharNode(UnaryNode):
+    def __init__(self, left: Union[Node,Token]):
+        super().__init__(left)
+
+    def shift(self):
+        tag = self.left.lexeme
+        return transitions_appender([tag])
---
+++ parsers.py
@@ -0,0 +1,122 @@
+from dataclasses import dataclass
+from typing import  Any, Dict
+from enum import Enum, auto
+from grammar_classes import Epsilon
+from state_class import State
+from automaton_class import Automaton
+from grammar_classes import Item, Terminal
+from serializer_class import Serializer
+from token_class import Token
+
+@dataclass
+class ParserSymbol:
+    name: Any
+    tag: Any = None
+
+class ParserTable(Serializer):
+    class Action(Enum): 
+        SHIFT = auto()
+        REDUCE = auto()
+        OK = auto()
+
+    def __init__(self):
+        self.start_symbol = None
+        self.start_state = None
+        self.dict_action: Dict[(Any, str), (ParserTable.Action, Any)] = dict()
+        self.dict_goto: Dict[(Any, str), Any] = dict()
+
+    def action(self, key, value=None):
+        if value is None:
+            return self.dict_action.get(key, (None, None))
+        if key in self.dict_action and not self.dict_action[key]== value:
+            raise ValueError(f"Conflict Occured")
+        self.dict_action[key] = value
+
+    def goto(self, key, value=None):
+        if value is None:
+            return self.dict_goto[key]
+        self.dict_goto[key] = value
+
+def parser_shr(tokens_list,ast_class,table:ParserTable,attr_decoder):
+        state_stack = [table.start_state]
+        symbol_stack = []
+        cursor = 0
+        while True:
+            token:Token=tokens_list[cursor]
+            state = state_stack[-1]
+            action, tag = table.action((state, token))
+            if action==table.Action.SHIFT:
+                state_stack.append(tag)
+                symbol = ParserSymbol(token.id,token)
+                symbol_stack.append(symbol)
+                cursor += 1
+            elif action ==table.Action.REDUCE:
+                to_remov_state=len(state_stack)-tag[1]    
+                state_stack=state_stack[:to_remov_state]
+                n_last_state=state_stack[-1]
+                to_remov_symbol=len(symbol_stack)-tag[1]    
+                s_to_reduce= symbol_stack[to_remov_symbol:]
+                symbol_stack = symbol_stack[:to_remov_symbol]
+                reduced=attr_decoder(tag[2],s_to_reduce,ast_class)
+                symbol_stack.append(ParserSymbol(tag[0],reduced))
+                state_stack.append(table.goto((n_last_state,tag[0])))
+            elif action ==table.Action.OK:
+                s=symbol_stack.pop()
+                return s.tag,[]
+            else:
+                col_error=0 if token.lexeme=='$' else token.col_no
+                line_error=0 if len(tokens_list)==1 else token.line_no
+                return None,[f'({line_error}, {col_error}) - SyntacticError: ERROR at or near "{token.lexeme}"']    
+
+def parser_lr1(grammar):
+        table = ParserTable()
+        table.start_symbol = grammar.start_symbol.name
+        S = grammar.nonTerminals(f"{repr(grammar.start_symbol)}*")
+        S != grammar.start_symbol
+        grammar.start_symbol = S
+
+        start_value = [Item(next(iter(grammar.start_symbol.productions)), 0, [grammar.eof])]
+        automaton=Automaton.to_deterministic(start_value,grammar.goto_lr1,grammar.clousure_lr1,grammar.state_constr,grammar.vocabulary)
+        print(f"automaton states {len(automaton.states)}")
+        table.start_state = automaton.start.name
+        states=automaton.states
+        c_is_reduce=0
+        for state in states:
+            state:State
+            for item in state.tag:
+                if item.is_reduce:
+                    c_is_reduce+=1
+                    if item.production.left == grammar.start_symbol:
+                        key = (state.name, grammar.eof.name)
+                        value = (table.Action.OK, 0)
+                        table.action(key,value)    
+                    else:
+                        for lookahead in item.lookaheads:
+                            key = (state.name, lookahead.name)
+                            attr=grammar.attr_encoder(item.production.attr)
+                            right_s_count=0
+                            if not isinstance(item.production.right.symbols[0],Epsilon):
+                                right_s_count=len(item.production.right.symbols)
+                            value = (table.Action.REDUCE, (item.production.left.name,right_s_count,attr))
+                            table.action(key,value)
+                else:
+                    symbol=item.next_symbol
+                    key = (state.name, symbol.name)
+                    if isinstance(symbol,Terminal):
+                        l=state[symbol.name]
+                        value =(table.Action.SHIFT, l.name)
+                        table.action(key,value)
+                    else:
+                        value = state[symbol.name].name
+                        table.goto(key, value)
+        print(f"action_count:{len(table.dict_action)}")
+        keys=list(table.dict_action.values())
+        s=0
+        r=0
+        for c in keys:
+            if c[0]==table.Action.REDUCE:r+=1
+            if c[0]==table.Action.SHIFT:s+=1
+        print(f"action_shift:{s}")
+        print(f"action_reduce:{r}") 
+        print(f"goto:{len(table.dict_goto)}")                
+        return table
