8ba8b191eda6ea009a4c930f543267a15ee72f6b
Reinaldo14 <reinaldobarrera9601@gmail.com>
2022-03-10 22:18:45 -0500

    ...

--- src/coolc.sh
+++ src/coolc.sh
@@ -5,4 +5,4 @@ OUTPUT_FILE=${INPUT_FILE:0: -2}mips
 echo "cool-compiler-2021"
 echo "Copyright (c) 2021: Reinaldo Barrera Travieso" 
 
-python main.py $INPUT_FILE $CIL_FILE $OUTPUT_FILE
+python main.py $INPUT_FILE $OUTPUT_FILE
